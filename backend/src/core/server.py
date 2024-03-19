b7b6610b04d18475e827a97c9303474af78e2770
Sean Friedowitz <sean@mozilla.ai>
2024-03-19 10:13:15 -0700

    update src.main and readme

---
+++ backend/src/core/server.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+from src.api import api_router
+from src.core.settings import settings
+
+app = FastAPI(title="MZAI Platform")
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the MZAI Platform!"}
+
+
+app.include_router(api_router, prefix=settings.API_V1_STR)

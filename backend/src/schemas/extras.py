fd326fe1492b4b5fec68548e430eb59b98b8819c
Sean Friedowitz <sean@mozilla.ai>
2024-03-20 19:17:07 -0700

    add back depl type to health

--- backend/src/schemas/extras.py
+++ backend/src/schemas/extras.py
@@ -1,5 +1,8 @@
 from pydantic import BaseModel, Field
 
+from src.settings import DeploymentType
+
 
 class Health(BaseModel):
+    deployment: DeploymentType = Field(..., example=DeploymentType.PRODUCTION)
     status: str = Field(..., example="Ok")

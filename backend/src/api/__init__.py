f8d4bdf0417755373f1fda27055e1032c105d2a3
Sean Friedowitz <sean@mozilla.ai>
2024-03-21 11:29:56 -0700

    fix bug in docker compose and lifespan

--- backend/src/api/__init__.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-
-from src.api.routes import health
-
-api_router = APIRouter()
-api_router.include_router(health.router, prefix="/health", tags=["health"])
--- backend/src/main.py
+++ backend/src/main.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from src.api import api_router
+from src.api.router import api_router
 from src.db import session_manager
 from src.settings import settings
 
@@ -14,7 +14,7 @@ async def lifespan(app: FastAPI):
     await session_manager.close()
 
 
-app = FastAPI(title="Platform Backend", lifepsan=lifespan)
+app = FastAPI(title="Platform Backend", lifespan=lifespan)
 
 
 app.include_router(api_router, prefix=settings.API_V1_STR)

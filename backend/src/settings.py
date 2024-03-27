f39fe3df45c163b7a3b5b425ab2529cbf9a7e94d
Sean Friedowitz <sean@mozilla.ai>
2024-03-26 18:08:51 -0700

    update docstrings

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -17,14 +17,14 @@ class Settings(BaseSettings):
     DEPLOYMENT_TYPE: DeploymentType = DeploymentType.LOCAL
 
     # Postgres
-    POSTGRES_PORT: int = 5432
     POSTGRES_HOST: str = "localhost"
+    POSTGRES_PORT: int = 5432
     POSTGRES_USER: str | None = None
     POSTGRES_PASSWORD: str | None = None
     POSTGRES_DB: str | None = None
 
     # Ray
-    RAY_HEAD_NODE_IP: str = "10.147.62.75"
+    RAY_HEAD_NODE_HOST: str = "localhost"
     RAY_HEAD_NODE_PORT: int = 8265
 
     @computed_field
@@ -32,10 +32,10 @@ class Settings(BaseSettings):
     def SQLALCHEMY_DATABASE_URL(self) -> URL:  # noqa: N802
         return URL.create(
             drivername="postgresql",
-            username=self.POSTGRES_USER,
-            password=self.POSTGRES_PASSWORD,
             host=self.POSTGRES_HOST,
             port=self.POSTGRES_PORT,
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
             database=self.POSTGRES_DB,
         )
 
--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,11 +1,11 @@
 import pytest
-import sqlalchemy
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from testcontainers.postgres import PostgresContainer
 
 from src.api.deps import get_db_session
-from src.db import BaseRecord, DatabaseSessionManager
+from src.db import BaseRecord
 from src.main import create_app
 from src.settings import settings
 
@@ -15,6 +15,7 @@ from src.settings import settings
 
 @pytest.fixture(scope="session")
 def test_db_engine():
+    """Initialize a Postgres test container as the DB engine for integration tests."""
     with PostgresContainer(
         "postgres:16-alpine",
         port=settings.POSTGRES_PORT,
@@ -23,35 +24,35 @@ def test_db_engine():
         dbname=settings.POSTGRES_DB,
     ) as postgres:
         url = postgres.get_connection_url()
-        engine = sqlalchemy.create_engine(url, echo=True)
+        engine = create_engine(url, echo=True)
         yield engine
 
 
 @pytest.fixture(scope="session", autouse=True)
 def initialize_db(test_db_engine):
-    # TODO: Run migrations here once using Alembic
+    """Create DB database/tables for the test suite.
+
+    # TODO: Run migrations here once switched over to Alembic.
+    """
     BaseRecord.metadata.create_all(test_db_engine)
 
 
 @pytest.fixture(scope="session")
 def test_app(test_db_engine):
+    """Create the FastAPI app bound to the test DB engine."""
     app = create_app(test_db_engine)
     return app
 
 
 @pytest.fixture(scope="function")
 def test_client(test_app):
+    """Create a test client for calling the FastAPI app."""
     with TestClient(test_app) as c:
         yield c
 
 
 @pytest.fixture(scope="function")
-def test_session_manager(test_db_engine):
-    return DatabaseSessionManager(test_db_engine)
-
-
-@pytest.fixture(scope="function")
-def test_db_session(test_session_manager):
+def test_db_session(test_db_engine):
     """Fixture to provide a clean DB session per test function.
 
     This method yields a session and rolls it back after test completion
@@ -59,7 +60,7 @@ def test_db_session(test_session_manager):
 
     Reference: https://dev.to/jbrocher/fastapi-testing-a-database-5ao5
     """
-    with test_session_manager.connect() as connection:
+    with test_db_engine.begin() as connection:
         try:
             session = Session(bind=connection)
             yield session
@@ -68,8 +69,13 @@ def test_db_session(test_session_manager):
 
 
 @pytest.fixture(scope="function", autouse=True)
-def session_override(test_app, test_db_session):
+def session_override(test_app, test_db_session) -> None:
+    """Override the FastAPI dependency injection for test DB sessions.
+
+    Reference: https://fastapi.tiangolo.com/he/advanced/testing-database/
+    """
+
     def get_db_session_override():
-        yield test_db_session[0]
+        yield test_db_session
 
     test_app.dependency_overrides[get_db_session] = get_db_session_override

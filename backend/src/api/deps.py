bd10e6e0a7c4728028ce2d1c1c8126c670f535e8
Sean Friedowitz <sean@mozilla.ai>
2024-04-01 11:29:49 -0700

    some review feedback

--- backend/src/api/deps.py
+++ backend/src/api/deps.py
@@ -8,9 +8,7 @@ from sqlalchemy.orm import Session
 from src.db import session_manager
 from src.repositories.finetuning import FinetuningJobRepository
 from src.services.finetuning import FinetuningService
-from src.utils import get_ray_job_client
-
-RayJobClientDep = Annotated[JobSubmissionClient, Depends(get_ray_job_client)]
+from src.settings import settings
 
 
 def get_db_session() -> Generator[Session, None, None]:
@@ -21,8 +19,9 @@ def get_db_session() -> Generator[Session, None, None]:
 DBSessionDep = Annotated[Session, Depends(get_db_session)]
 
 
-def get_finetuning_service(session: DBSessionDep, ray_client: RayJobClientDep) -> FinetuningService:
+def get_finetuning_service(session: DBSessionDep) -> FinetuningService:
     job_repo = FinetuningJobRepository(session)
+    ray_client = JobSubmissionClient(settings.RAY_DASHBOARD_URL)
     return FinetuningService(job_repo, ray_client)
 
 

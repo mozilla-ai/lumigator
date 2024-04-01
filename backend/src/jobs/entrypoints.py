608eaf020b50aabe0b8771d5588d910f61df62bf
Sean Friedowitz <sean@mozilla.ai>
2024-04-01 09:02:09 -0700

    make interface for finetuning job

--- backend/src/jobs/entrypoints.py
+++ backend/src/jobs/entrypoints.py
@@ -1,29 +1,38 @@
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 from typing import Any
 
+from ray.job_submission import JobSubmissionClient
 
+
+@dataclass(kw_only=True)
 class RayJobEntrypoint(ABC):
+    runtime_env: dict[str, Any] | None = None
+    num_cpus: int | float | None = None
+    num_gpus: int | float | None = None
+    memory: int | float | None = None
+
     @property
     @abstractmethod
     def command(self) -> str:
         pass
 
-    @property
-    def runtime_env(self) -> dict[str, Any] | None:
-        return None
 
-    @property
-    def num_cpus(self) -> int | float | None:
-        return None
-
-    @property
-    def num_gpus(self) -> int | float | None:
-        return None
-
-    @property
-    def memory(self) -> int | float | None:
-        return None
-
-
+@dataclass(kw_only=True)
 class FinetuningJobEntrypoint(RayJobEntrypoint):
-    pass
+    config: dict[str, Any]
+
+    @property
+    def command(self) -> str:
+        # TODO: Dummy entrypoint logic that needs to be updated for real
+        return "echo 'Hello from Ray!'"
+
+
+def submit_ray_job(client: JobSubmissionClient, entrypoint: RayJobEntrypoint) -> str:
+    return client.submit_job(
+        entrypoint=entrypoint.command,
+        entrypoint_num_cpus=entrypoint.num_cpus,
+        entrypoint_num_gpus=entrypoint.num_gpus,
+        entrypoint_memory=entrypoint.memory,
+        runtime_env=entrypoint.runtime_env,
+    )

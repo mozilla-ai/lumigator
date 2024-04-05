9de67938ab73b6d90a6c3a1c09c61f1b27c57cd1
Aaron Gonzales <aaron@mozilla.ai>
2024-04-05 15:35:55 -0600

    updating folder structure as discussed in live session

--- Makefile
+++ Makefile
@@ -22,3 +22,20 @@ ci-fmt: ci-lint
 ci-tests:
 	pants test ::
 
+
+show-all-major-targets::
+	@echo "------shell_command targets-------"
+	pants --filter-target-type=shell_command list ::
+	pants --filter-target-type=run_shell_command list ::
+	@echo "------docker image targets-------"
+	pants --filter-target-type=docker_image list ::
+	@echo "------python targets-------"
+	pants --filter-target-type=python_distribution list ::
+	pants --filter-target-type=pex_binary list ::
+
+	@echo "------archive targets-------"
+
+	pants --filter-target-type=archive list ::
+
+	@echo "this is not an exhaustive list, just a convienience."
+
---
+++ model_builder/python/mzai/model_builder/backend/api/deps.py
@@ -0,0 +1,28 @@
+from collections.abc import Generator
+from typing import Annotated
+
+from fastapi import Depends
+from ray.job_submission import JobSubmissionClient
+from sqlalchemy.orm import Session
+
+from mzai.model_builder.backend.db import session_manager
+from mzai.model_builder.backend.repositories.finetuning import FinetuningJobRepository
+from mzai.model_builder.backend.services.finetuning import FinetuningService
+from mzai.model_builder.backend.settings import settings
+
+
+def get_db_session() -> Generator[Session, None, None]:
+    with session_manager.session() as session:
+        yield session
+
+
+DBSessionDep = Annotated[Session, Depends(get_db_session)]
+
+
+def get_finetuning_service(session: DBSessionDep) -> FinetuningService:
+    job_repo = FinetuningJobRepository(session)
+    ray_client = JobSubmissionClient(settings.RAY_DASHBOARD_URL)
+    return FinetuningService(job_repo, ray_client)
+
+
+FinetuningServiceDep = Annotated[FinetuningService, Depends(get_finetuning_service)]

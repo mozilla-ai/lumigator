7634c7a8b122aba4c80b1b70b1ee78ee78220237
Aaron Gonzales <aaron@mozilla.ai>
2024-04-05 11:08:36 -0600

    updated build files via linting

--- src/infra/mzai/backend/docker/docker-compose.sh
+++ src/infra/mzai/backend/docker/docker-compose.sh
@@ -3,30 +3,30 @@
 COMPOSE_FILE=docker-compose.yaml
 ENV_FILE=.env.local
 
-
-down(){
-  docker compose -f ${COMPOSE_FILE} --env-file ${ENV_FILE} down
+down() {
+	docker compose -f ${COMPOSE_FILE} --env-file ${ENV_FILE} down
 }
 
-up(){
-  down
-  docker compose -f ${COMPOSE_FILE} --env-file ${ENV_FILE} up -d
+up() {
+	down
+	docker compose -f ${COMPOSE_FILE} --env-file ${ENV_FILE} up -d
 }
-
 
 case "$1" in
-    build)
-        echo "Downloading $2..."
-        docker compose -f ${COMPOSE_FILE} --env-file ${ENV_FILE} build;;
+build)
+	echo "Downloading $2..."
+	docker compose -f ${COMPOSE_FILE} --env-file ${ENV_FILE} build
+	;;
 
-     up)
-        up;;
+up)
+	up
+	;;
 
-    down)
-        down ;;
+down)
+	down
+	;;
 
-      *)
-        echo "bad arg" ;;
+*)
+	echo "bad arg"
+	;;
 esac
-
-

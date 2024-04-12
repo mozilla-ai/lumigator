b1d4cf24bbb82c09d854552997a8568430c6c2ef
Aaron Gonzales <aaron@mozilla.ai>
2024-04-12 11:00:05 -0600

    updated ide / python setup

--- pants_tools/bootstrap_python.sh
+++ pants_tools/bootstrap_python.sh
@@ -9,17 +9,27 @@ DEBIAN="cpython-${PY_VERSION}+${TAG}-x86_64-unknown-linux-gnu-pgo+lto-full.tar.z
 DARWIN="cpython-${PY_VERSION}+${TAG}-aarch64-apple-darwin-pgo+lto-full.tar.zst"
 
 LOCAL_PYTHON_PATH="$HOME/workspace/.pythoninterpreters/python${PY_VERSION}"
+INTERPRETER="${LOCAL_PYTHON_PATH}/python/install/bin/python3"
 mkdir -p "$LOCAL_PYTHON_PATH"
-cd "$LOCAL_PYTHON_PATH"
+pushd "$LOCAL_PYTHON_PATH"
 (set +o pipefail ; rm -r *python*)
 
 arch=${1:-Darwin}
 if [[ $arch == "Darwin" ]]; then
+  echo "installing macos interpreter"
   wget "$URL/$DARWIN"
   tarbase=${DARWIN%.*}
   zstd -d "$DARWIN"
   tar xzf "${tarbase}"
+  rm cpython*
+  cd "$WORKSPACE/mzai-platform/"
+
+  macos_tags_file="$WORKSPACE/mzai-platform/pants_tools/macosx_14_pex_platform_tags.json"
+	cat "$macos_tags_file" | jq '.path = "'"$INTERPRETER"'"' > "${macos_tags_file}.new"
+	mv "${macos_tags_file}.new" "$macos_tags_file"
+
 elif [[ $arch  == "debian" ]]; then
+  echo "installing debian interpreter"
   wget "$URL/$DEBIAN"
   tar -axvf "$DEBIAN"
 else

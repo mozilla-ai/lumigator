1bf7c3766b2672a5ca0f8b326ec3d1e7aa494763
Sean Friedowitz <sean@mozilla.ai>
2024-04-26 12:10:55 -0700

    add primtive event message via http call to backend

--- platform/python/mzai/jobrunner/runner.py
+++ platform/python/mzai/jobrunner/runner.py
@@ -1,9 +1,38 @@
-from mzai.schemas.jobs import JobConfig
+import random
+
+from mzai.jobrunner.events import send_job_event
+from mzai.schemas.jobs import JobConfig, JobStatus
 
 
 class JobRunner:
+    """Generic runner for platform jobs.
+
+    Accepts a `JobConfig` which defines the type of job to run and its parameters,
+    and dispatches to the appropriate implementation internally.
+    """
+
+    def __init__(self):
+        pass
+
+    def on_begin(self, config: JobConfig) -> None:
+        send_job_event(config.job_id, config.job_type, JobStatus.RUNNING, detail="Job started.")
+
+    def on_success(self, config: JobConfig) -> None:
+        send_job_event(config.job_id, config.job_type, JobStatus.SUCCEEDED, detail="Job finished.")
+
+    def on_failure(self, config: JobConfig, exception: Exception) -> None:
+        send_job_event(
+            config.job_id,
+            config.job_type,
+            JobStatus.FAILED,
+            detail=f"Job failed with exception: {exception}.",
+        )
+
     def run(self, config: JobConfig):
+        self.on_begin(config)
         try:
-            print(config)
+            if random.random() >= 0.5:
+                raise ValueError("Job went crazy!")
+            self.on_success(config)
         except Exception as e:
-            print(e)
+            self.on_failure(config, e)

e4ef85dd19af3d415c8aab84ae1d1a77cb43de9f
Kyle White <kylewhite@gmail.com>
2024-06-25 17:27:57 +0100

    update submission

--- platform/python/mzai/backend/jobs/submission.py
+++ platform/python/mzai/backend/jobs/submission.py
@@ -1,15 +1,14 @@
+import json
 from abc import ABC
 from dataclasses import dataclass
 from typing import Any
 
 from ray.job_submission import JobSubmissionClient
 
-from mzai.schemas.jobs import JobConfig
-
 
 @dataclass(kw_only=True)
 class RayJobEntrypoint(ABC):
-    config: JobConfig
+    config: dict[str, Any] | None = None
     runtime_env: dict[str, Any] | None = None
     num_cpus: int | float | None = None
     num_gpus: int | float | None = None
@@ -17,7 +16,7 @@ class RayJobEntrypoint(ABC):
 
     @property
     def command(self) -> str:
-        return f"lm-buddy evaluate --config '{self.config.model_dump_json()}'"
+        return f"lm-buddy evaluate --config '{json.dumps(self.config)}'"
 
 
 def submit_ray_job(client: JobSubmissionClient, entrypoint: RayJobEntrypoint) -> str:
@@ -27,5 +26,5 @@ def submit_ray_job(client: JobSubmissionClient, entrypoint: RayJobEntrypoint) ->
         entrypoint_num_gpus=entrypoint.num_gpus,
         entrypoint_memory=entrypoint.memory,
         runtime_env=entrypoint.runtime_env,
-        submission_id=str(entrypoint.config.job_id),  # Use the record ID for the Ray submission
+        # submission_id=str(entrypoint.config.job_id),  # Use the record ID for the Ray submission
     )

9fbf174b50c1070bbc6d716b1dace08c2d50ed76
Davide Eynard <davide@mozilla.ai>
2024-07-18 19:07:56 +0100

    Added comments to RayJobEntryPoint

--- lumigator/python/mzai/backend/jobs/submission.py
+++ lumigator/python/mzai/backend/jobs/submission.py
@@ -10,6 +10,15 @@ from mzai.schemas.jobs import JobConfig
 
 @dataclass(kw_only=True)
 class RayJobEntrypoint(ABC):
+    """
+    A RayJobEntrypoint boils down to a command which is passed a given config
+    and submitted as a ray job.
+
+    Currently the only command we run is `lm-buddy evaluate`, but this can
+    be parametrised to support both different parameters (e.g. `lm-buddy
+    evaluate lm-harness`, `lm-buddy finetune`, etc) or even different commands.
+    """
+
     config: JobConfig
     runtime_env: dict[str, Any] | None = None
     num_cpus: int | float | None = None
@@ -18,6 +27,9 @@ class RayJobEntrypoint(ABC):
 
     @property
     def command(self) -> str:
+        """
+        The command ran by the ray job, which is passed a JSON-serialized config
+        """
         return f"lm-buddy evaluate huggingface --config '{json.dumps(self.config.args)}'"
 
 

1de2c47011221588061fe56746e593b4b0087694
Vicki Boykis <vicki@mozilla.ai>
2024-07-29 14:35:23 -0400

    working endpoint

--- lumigator/python/mzai/backend/services/completions.py
+++ lumigator/python/mzai/backend/services/completions.py
@@ -1,4 +1,6 @@
+import mistralai.client
 from mistralai.client import MistralClient
+from mistralai.models.chat_completion import ChatMessage
 from abc import ABC, abstractmethod
 import os
 
@@ -17,16 +19,19 @@ class MistralCompletionService(CompletionService):
         self.temperature = 0.7
         self.top_p = 1
 
+    def get_models(self) -> mistralai.client.ModelList:
+        response = self.client.list_models()
+
+        return response
+
     def get_completions_response(self, prompt: str) -> str:
         prompt = f"Summarize the following text: {prompt}"
 
-        response = self.client.completion(
+        response = self.client.chat(
             model=self.model,
-            prompt=prompt,
+            messages=[ChatMessage(role="user", content=f"Summarize the following: {prompt}")],
+            temperature=self.temperature,
+            max_tokens=self.max_tokens,
+            top_p=self.top_p,
         )
-
         return response.choices[0].message.content
-
-
-ms = MistralCompletionService()
-ms.get_completions_response("testing")

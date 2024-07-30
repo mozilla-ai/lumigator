c430a61e1f325e471a80a0f89c365a491f0c7f3a
Vicki Boykis <vicki@mozilla.ai>
2024-07-30 12:50:33 -0400

    Mistral and OpenAI completions endpoint for Lumigator  (#121)
    * endpoint
    
    * completions
    
    * working endpoint
    
    * openai
    
    * mistral + openai
    
    * working openai completion
    
    * remove notebook
    
    * change temp to 1

---
+++ lumigator/python/mzai/schemas/completions.py
@@ -0,0 +1,13 @@
+import datetime
+from enum import Enum
+from uuid import UUID
+
+from pydantic import BaseModel
+
+
+class CompletionResponse(BaseModel):
+    text: str
+
+
+class CompletionRequest(BaseModel):
+    text: str

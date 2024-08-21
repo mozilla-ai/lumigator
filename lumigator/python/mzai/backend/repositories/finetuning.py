0bff5f342d5d1012976d497f80ea472a7a27ff22
Vicki Boykis <vicki@mozilla.ai>
2024-08-21 08:38:55 -0400

    Remove finetuning and events routes (#155)
    * remove finetuning and events routes
    
    * remove finetuning service

--- lumigator/python/mzai/backend/repositories/finetuning.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import Session
-
-from mzai.backend.records.finetuning import FinetuningJobRecord
-from mzai.backend.repositories.base import BaseRepository
-
-
-class FinetuningJobRepository(BaseRepository[FinetuningJobRecord]):
-    def __init__(self, session: Session):
-        super().__init__(FinetuningJobRecord, session)

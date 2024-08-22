f5e049fadf6de555eabd24f547ec7a3b80815750
Aaron Gonzales <aaron@mozilla.ai>
2024-08-22 16:44:16 -0600

    adds pants wrapper for helping parametrization and moves all python setup/management to the dedicated 'bootstrap_python.sh' script

--- pants_tools/bootstrap_python.sh
+++ pants_tools/bootstrap_python.sh
@@ -1,36 +1,99 @@
 #!/usr/bin/env bash
 set -eou pipefail
+
+# Requires curl.
+
+function check_if_installed() {
+	tool=$1
+	test=$(command -v "$tool")
+	if [[ -z $test ]]; then
+		echo "0"
+	else
+		echo "$test"
+	fi
+}
+
+plat=$(uname -o)
+PLAT=${plat,,} # lowercase
 PY_VERSION=${MZAI_PY_VERISON:-3.11.9}
-# this is the release tag - see https://github.com/indygreg/python-build-standalone/releases/
-TAG="20240415"
-URL="https://github.com/indygreg/python-build-standalone/releases/download/$TAG"
-DEBIAN="cpython-${PY_VERSION}+${TAG}-x86_64-unknown-linux-gnu-pgo+lto-full.tar.zst"
-DARWIN="cpython-${PY_VERSION}+${TAG}-aarch64-apple-darwin-pgo+lto-full.tar.zst"
-REPOROOT=$(git rev-parse --show-toplevel)
-LOCAL_PYTHON_PATH="$REPOROOT/.python/python${PY_VERSION}"
-INTERPRETER="${LOCAL_PYTHON_PATH}/python/install/bin/python3"
-echo "cleaning previous installation at $LOCAL_PYTHON_PATH"
-rm -r "$LOCAL_PYTHON_PATH" || true # hack for make to have this  always return true.
-mkdir -p "$LOCAL_PYTHON_PATH"
-pushd "$LOCAL_PYTHON_PATH"
+PYTHON_INSTALLED=$(check_if_installed python)
+CUDA_AVAILABLE=$(check_if_installed nvcc)
+TORCH_VERSION="2.4.0"
+TORCH_CUDA_VERSION="cu121"
+UV_INSTALLED=$(check_if_installed uv)
+VENVNAME="mzaivenv"
 
-arch=${1:-Darwin}
-if [[ $arch == "Darwin" ]]; then
-	echo "installing macos interpreter from $URL/$DARWIN"
-	wget -nv "$URL/$DARWIN"
-	tarbase=${DARWIN%.*}
-	zstd -d "$DARWIN"
-	tar xzf "${tarbase}"
-	rm cpython*
-	cd "$REPOROOT"
-	printf "interpreter is available at\n%s\n and is not on your PATH. use it explicitly if you'd like\n" "$INTERPRETER"
+if [[ $PLAT == 'gnu/linux' ]]; then
+	echo "linux platform detected"
 
-elif [[ $arch == "GNU/Linux" ]]; then
-	echo "installing debian interpreter"
-	wget -nv "$URL/$DEBIAN"
-	tar -axf "$DEBIAN"
-	rm cpython* || true
-	printf "interpreter is available at\n%s\n and is not on your PATH. use it explicitly if you'd like\n" "$INTERPRETER"
+	if [[ "$CUDA_AVAILABLE" != 0 ]]; then
+		echo "nvcc found; configuring with CUDA"
+		PYTHON_INSTALL_DIR=/opt/python
+		PY_NAME=cpython-3.11.9-linux-x86_64-gnu
+		PYTHON=${PYTHON_INSTALL_DIR}/${PY_NAME}/bin/python3
+		echo "torch[cuda]==${TORCH_VERSION}+${TORCH_CUDA_VERSION}" >tmp_overrides.txt
+		UV_ARGS="--extra-index-url https://download.pytorch.org/whl/${TORCH_CUDA_VERSION} --index-strategy=unsafe-best-match --override tmp_overrides.txt"
+	else
+		PYTHON_INSTALL_DIR=/opt/python
+		PY_NAME=cpython-3.11.9-linux-x86_64-gnu
+		PYTHON=${PYTHON_INSTALL_DIR}/${PY_NAME}/bin/python3
+		echo "torch==2.4.0+cpu" >tmp_overrides.txt
+		UV_ARGS="--extra-index-url https://download.pytorch.org/whl/cpu --index-strategy=unsafe-best-match --override tmp_overrides.txt"
+	fi
 else
-	echo "$arch was passed but isn't valid. exiting!"
+	echo "Darwin setup"
+	PYTHON_INSTALL_DIR=.python
+	PY_NAME=cpython-3.11.9-macos-aarch64-none
+	PYTHON=${PYTHON_INSTALL_DIR}/${PY_NAME}/bin/python3
+	echo "torch==2.4.0" >tmp_overrides.txt
+	UV_ARGS="--strict"
 fi
+
+function install_uv() {
+	if [[ "$UV_INSTALLED" == 0 ]]; then
+		echo "installing UV from source"
+		curl -LsSf https://astral.sh/uv/install.sh | sh
+		UV_BIN="$HOME/.cargo/bin/uv"
+	else
+		echo "UV is available at $UV_INSTALLED"
+		UV_BIN=${UV_INSTALLED}
+	fi
+	export UV_BIN
+}
+
+function install_python() {
+	if [[ "$PYTHON_INSTALLED" != 0 ]]; then
+		if [[ $("$PYTHON_INSTALLED" --version) == "Python ${PY_VERSION}" ]]; then
+			echo "Local compatible python found at $PYTHON_INSTALLED"
+			PYTHON="$PYTHON_INSTALLED"
+		else
+			echo "Local python found at $PYTHON_INSTALLED is not the right version - installing from UV"
+			"$UV_BIN" python install "$PY_VERSION" --python-preference only-managed
+		fi
+	else
+		echo "installing python interpreter from UV"
+		"$UV_BIN" python install "$PY_VERSION" --python-preference only-managed
+	fi
+}
+
+function install_venv() {
+	if [[ ! -e "$VENVNAME" ]]; then
+		echo "making a virtual env and installing the suite of deps into it."
+		"$UV_BIN" venv "$VENVNAME" --seed --python "$PYTHON"
+		# shellcheck source=/dev/null
+		source "$VENVNAME/bin/activate"
+		"$UV_BIN" pip install -r 3rdparty/python/pyproject.toml "$UV_ARGS"
+		rm tmp_overrides.txt || true
+	else
+		echo "found a directory where the venv is supposed to be - remove it if you want to install there."
+	fi
+	echo "Activate the venv with the following command:"
+	echo ""
+	echo "source $VENVNAME/bin/activate"
+}
+
+UV_PYTHON_INSTALL_DIR=$PYTHON_INSTALL_DIR
+export UV_PYTHON_INSTALL_DIR
+install_uv
+install_python
+install_venv

a715b66ca6fa88fd8eabbd32a9be7ab5202a52fb
Javier Torres <javier@mozilla.ai>
2024-09-23 08:28:04 +0200

    Fix postgres startup

--- .devcontainer/start_postgres.sh
+++ .devcontainer/start_postgres.sh
@@ -1,8 +1,11 @@
 mkdir -p /etc/lumigator/data/db/
 cd /etc/lumigator/data/db
 
-su -c "psql -c \"CREATE USER ${POSTGRES_USER} WITH PASSWORD '${POSTGRES_PASSWORD}';\"" postgres
-su -c "psql -c \"CREATE DATABASE ${POSTGRES_DB} OWNER ${POSTGRES_USER}\"" postgres
-su -c "psql -c \"GRANT ALL ON SCHEMA public TO ${POSTGRES_USER};\"" postgres
 /usr/lib/postgresql/15/bin/postgres -D /etc/postgresql/15/main/ &
 echo $! > /etc/lumigator/data/db/postgres.pid
+
+until psql -c "SELECT current_timestamp - pg_postmaster_start_time();"; do echo "Waiting for postgres"; sleep 3; done
+
+psql -c "CREATE USER ${POSTGRES_USER} WITH PASSWORD '${POSTGRES_PASSWORD}';"
+psql -c "CREATE DATABASE ${POSTGRES_DB} OWNER ${POSTGRES_USER}"
+psql -c "GRANT ALL ON SCHEMA public TO ${POSTGRES_USER};"

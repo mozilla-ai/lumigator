9908e669620453444f31ca82bf49dc45b4537827
Javier Torres <javier@mozilla.ai>
2024-09-24 16:02:55 +0200

    Use plain pytest

--- lumigator/python/mzai/sdk/healthcheck.py
+++ lumigator/python/mzai/sdk/healthcheck.py
@@ -1,8 +1,5 @@
 import requests
 
-from sdk.core import LumigatorClient as cl
-
-
 class HealthCheck():
 
     def __init__(self):
--- lumigator/python/mzai/sdk/tests/test_sdk.py
+++ lumigator/python/mzai/sdk/tests/test_sdk.py
@@ -19,13 +19,13 @@ class MockResponse:
 @pytest.fixture(scope="function")
 def mock_requests_response():
     """Mocks calls to `requests.Response`."""
-    with mock.patch(requests.Response) as resp_mock:
+    with mock.patch("requests.Response") as resp_mock:
         yield resp_mock
 
 @pytest.fixture(scope="function")
 def mock_requests(mock_requests_response):
     """Mocks calls to `requests.request`."""
-    with mock.patch(requests.request) as req_mock:
+    with mock.patch("requests.request") as req_mock:
         req_mock.return_value = mock_requests_response
         yield req_mock
 

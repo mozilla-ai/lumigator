1f54fc9c6cb73655d591002dff591a1e8babbe40
Vicki Boykis <vicki@mozilla.ai>
2024-09-26 14:30:05 -0400

    evaluate cli changes

--- lumigator/python/mzai/evaluator/cli/evaluate.py
+++ lumigator/python/mzai/evaluator/cli/evaluate.py
@@ -19,29 +19,29 @@ def group() -> None:
 @click.option("--config", type=str)
 def lm_harness_command(config: str) -> None:
     config = parse_config_option(LMHarnessJobConfig, config)
-    buddy = Evaluator()
-    buddy.evaluate(config)
+    evaluate = Evaluator()
+    evaluate.evaluate(config)
 
 
 @group.command("prometheus", help="Run the prometheus evaluation job.")
 @click.option("--config", type=str)
 def prometheus_command(config: str) -> None:
     config = parse_config_option(PrometheusJobConfig, config)
-    buddy = Evaluator()
-    buddy.evaluate(config)
+    evaluate = Evaluator()
+    evaluate.evaluate(config)
 
 
 @group.command("ragas", help="Run the ragas evaluation job.")
 @click.option("--config", type=str)
 def ragas_command(config: str) -> None:
     config = parse_config_option(RagasJobConfig, config)
-    buddy = Evaluator()
-    buddy.evaluate(config)
+    evaluate = Evaluator()
+    evaluate.evaluate(config)
 
 
 @group.command("huggingface", help="Run the HuggingFace evaluation job.")
 @click.option("--config", type=str)
 def huggingface_command(config: str) -> None:
     config = parse_config_option(HuggingFaceEvalJobConfig, config)
-    buddy = Evaluator()
-    buddy.evaluate(config)
+    evaluate = Evaluator()
+    evaluate.evaluate(config)

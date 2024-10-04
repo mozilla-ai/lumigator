4c42757ea5f8c4cdea7b515075f3d449e0a161cc
Peter Wilson <peter@mozilla.ai>
2024-10-04 11:10:39 +0100

    spacing

--- lumigator/python/mzai/sdk/tests/test_sdk.py
+++ lumigator/python/mzai/sdk/tests/test_sdk.py
@@ -150,6 +150,7 @@ def test_get_jobs_ok(mock_requests_response, mock_requests, lumi_client):
     assert jobs[0].message == "I am the message"
     assert jobs[1].message == "I am another message"
 
+
 def test_get_jobs_none(mock_requests_response, mock_requests, lumi_client):
     mock_requests_response.status_code = 200
     mock_requests_response.json = lambda: json.loads("[]")
@@ -157,6 +158,7 @@ def test_get_jobs_none(mock_requests_response, mock_requests, lumi_client):
     jobs = lumi_client.get_jobs()
     assert jobs is not None
     assert jobs == []
+
 
 def test_get_job_ok(mock_requests_response, mock_requests, lumi_client):
     mock_requests_response.status_code = 200
@@ -175,6 +177,7 @@ def test_get_job_ok(mock_requests_response, mock_requests, lumi_client):
     assert job.type == "SUBMISSION"
     assert job.submission_id == job_id
 
+
 def test_get_job_id_does_not_exist(mock_requests_response, mock_requests, lumi_client):
     mock_requests_response.status_code = HTTPStatus.NOT_FOUND
     mock_requests_response.json = lambda: None
@@ -184,4 +187,3 @@ def test_get_job_id_does_not_exist(mock_requests_response, mock_requests, lumi_c
     # We expect the SDK to handle the 404 and return None.
     job = lumi_client.get_job("6f6487ac-7170-4a11-af7a-0f6db1ec9a74")
     assert job is None
-

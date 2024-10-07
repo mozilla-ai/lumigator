6888c5386716936ecd3c48a09c0b44870ccbc571
Peter Wilson <peter@mozilla.ai>
2024-10-07 16:20:00 +0100

    Remove TODO, add TODO.

--- lumigator/python/mzai/sdk/tests/conftest.py
+++ lumigator/python/mzai/sdk/tests/conftest.py
@@ -19,7 +19,6 @@ def mock_requests(mock_requests_response):
     """Mocks calls to `requests.request`."""
     with mock.patch("requests.request") as req_mock:
         req_mock.return_value = mock_requests_response
-        # TODO write a side effect to check the url
         yield req_mock
 
 

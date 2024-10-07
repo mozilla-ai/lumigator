fb3686b198890d606ccbec0b4d3c3565c8788502
Peter Wilson <peter@mozilla.ai>
2024-10-07 19:39:52 +0100

    Updated tests to use conftest.py references for shared/data

--- lumigator/python/mzai/sdk/tests/conftest.py
+++ lumigator/python/mzai/sdk/tests/conftest.py
@@ -1,7 +1,9 @@
+import importlib.resources
 import unittest.mock as mock
 
 import pytest
 
+from importlib.resources.abc import Traversable
 from lumigator import LumigatorClient
 
 LUMI_HOST = "localhost"
@@ -25,3 +27,22 @@ def mock_requests(mock_requests_response):
 @pytest.fixture(scope="session")
 def lumi_client() -> LumigatorClient:
     return LumigatorClient(LUMI_HOST)
+
+
+def json_data_path() -> Traversable:
+    return importlib.resources.files("sdk.tests") / "data"
+
+
+@pytest.fixture(scope="session")
+def json_data_deployments() -> Traversable:
+    return json_data_path() / "deployments.json"
+
+
+@pytest.fixture(scope="session")
+def json_data_jobs() -> Traversable:
+    return json_data_path() / "jobs.json"
+
+
+@pytest.fixture(scope="session")
+def json_data_job() -> Traversable:
+    return json_data_path() / "job.json"

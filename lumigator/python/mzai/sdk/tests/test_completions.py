6469580060a609d0f7d3e7986c7f57db45af81b2
Peter Wilson <peter@mozilla.ai>
2024-10-08 10:48:03 +0100

    add tests for basic openai/mistral submission of a completion

--- lumigator/python/mzai/sdk/tests/test_completions.py
+++ lumigator/python/mzai/sdk/tests/test_completions.py
@@ -55,3 +55,39 @@ def test_get_completion_invalid_text_spacey(
 
     with raises(ValueError):
         lumi_client.completions.get_completion("openai", "  ")
+
+
+def test_get_completion_openai_ok(
+    mock_requests_response, mock_requests, lumi_client, mock_vendor_data
+):
+    # Mock vendors response and prep the cache.
+    # As we don't have this mocked during client creation.
+    mock_requests_response.status_code = 200
+    mock_requests_response.json = lambda: json.loads(mock_vendor_data)
+    lumi_client.completions.get_vendors()
+
+    # Now set the mock response data up as we'd expect it for the actual request
+    mock_requests_response.status_code = 200
+    mock_requests_response.json = lambda: json.loads(f'{{"text": "thanks"}}')
+
+    response = lumi_client.completions.get_completion("openai", "please")
+    assert response is not None
+    assert response.text == "thanks"
+
+def test_get_completion_mistral_ok(
+    mock_requests_response, mock_requests, lumi_client, mock_vendor_data
+):
+    # Mock vendors response and prep the cache.
+    # As we don't have this mocked during client creation.
+    mock_requests_response.status_code = 200
+    mock_requests_response.json = lambda: json.loads(mock_vendor_data)
+    lumi_client.completions.get_vendors()
+
+    # Now set the mock response data up as we'd expect it for the actual request
+    mock_requests_response.status_code = 200
+    mock_requests_response.json = lambda: json.loads(f'{{"text": "thanks"}}')
+
+    response = lumi_client.completions.get_completion("mistral", "please")
+    assert response is not None
+    assert response.text == "thanks"
+

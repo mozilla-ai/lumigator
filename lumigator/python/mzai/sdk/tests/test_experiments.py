fda082997a055ed5815da253d40721464ff6f903
Peter Wilson <peter@mozilla.ai>
2024-10-10 13:43:12 +0100

    Fix existing experiment tests

--- lumigator/python/mzai/sdk/tests/test_experiments.py
+++ lumigator/python/mzai/sdk/tests/test_experiments.py
@@ -1,11 +1,13 @@
-from helpers import load_request, load_response
+from tests.helpers import load_json
 
 
 def test_get_experiments_ok(
     mock_requests_response, mock_requests, lumi_client, json_data_experiments
 ):
     mock_requests_response.status_code = 200
-    load_response(mock_requests_response, json_data_experiments)
+    data = load_json(json_data_experiments)
+    mock_requests_response.json = lambda: data
+
     experiments_ret = lumi_client.experiments.get_experiments()
     assert experiments_ret is not None
 
@@ -14,7 +16,8 @@ def test_get_experiment_ok(
     mock_requests_response, mock_requests, lumi_client, json_data_experiment
 ):
     mock_requests_response.status_code = 200
-    load_response(mock_requests_response, json_data_experiment)
+    data = load_json(json_data_experiment)
+    mock_requests_response.json = lambda: data
     experiment_ret = lumi_client.experiments.get_experiment("daab39ac-be9f-4de9-87c0-c4c94b297a97")
     assert experiment_ret is not None
 
@@ -27,10 +30,11 @@ def test_create_experiment_ok_simple(
     json_data_experiment_post_simple,
 ):
     mock_requests_response.status_code = 200
-    load_response(mock_requests_response, json_data_experiment_post_response)
-    experiment_ret = lumi_client.experiments.create_experiment(
-        load_request(json_data_experiment_post_simple)
-    )
+    data = load_json(json_data_experiment_post_response)
+    mock_requests_response.json = lambda: data
+
+    experiment_json = load_json(json_data_experiment_post_simple)
+    experiment_ret = lumi_client.experiments.create_experiment(experiment_json)
     assert experiment_ret is not None
 
 
@@ -42,8 +46,10 @@ def test_create_experiment_ok_all(
     json_data_experiment_post_all,
 ):
     mock_requests_response.status_code = 200
-    load_response(mock_requests_response, json_data_experiment_post_response)
-    experiment_ret = lumi_client.experiments.create_experiment(
-        load_request(json_data_experiment_post_all)
-    )
+    data = load_json(json_data_experiment_post_response)
+    mock_requests_response.json = lambda: data
+
+    experiment_json = load_json(json_data_experiment_post_all)
+    experiment_ret = lumi_client.experiments.create_experiment(experiment_json)
+
     assert experiment_ret is not None

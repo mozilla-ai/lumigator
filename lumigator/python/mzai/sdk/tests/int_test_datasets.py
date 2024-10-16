2243099e77280717c74088812d9af73604bd033e
Javier Torres <javier@mozilla.ai>
2024-10-16 10:25:33 +0200

    Review rework

--- lumigator/python/mzai/sdk/tests/int_test_datasets.py
+++ lumigator/python/mzai/sdk/tests/int_test_datasets.py
@@ -13,9 +13,13 @@ def test_get_datasets_remote_ok(lumi_client):
 
 def test_dataset_lifecycle_remote_ok(lumi_client, hf_data):
     with Path.open(hf_data) as file:
+        datasets = lumi_client.datasets.get_datasets()
+        assert datasets.total is 0
         dataset = lumi_client.datasets.create_dataset(dataset=file, format=DatasetFormat.EXPERIMENT)
         datasets = lumi_client.datasets.get_datasets()
-        assert datasets.total is not 0
+        assert datasets.total is 1
         dataset = lumi_client.datasets.get_dataset(datasets.items[0].id)
         assert dataset is not None
         lumi_client.datasets.delete_dataset(datasets.items[0].id)
+        datasets = lumi_client.datasets.get_datasets()
+        assert datasets.total is 0

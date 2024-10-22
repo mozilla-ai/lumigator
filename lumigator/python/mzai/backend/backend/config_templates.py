4d547afe39115630b851390831105fbae3c4cec6
Vicki Boykis <vicki@mozilla.ai>
2024-10-22 11:10:49 -0400

    add models

--- lumigator/python/mzai/backend/backend/config_templates.py
+++ lumigator/python/mzai/backend/backend/config_templates.py
@@ -97,13 +97,16 @@ oai_infer_template = """{{
 }}"""
 
 config_infer_template = {
-    "hf://facebook/bart-large-cnn": bart_infer_template,
-    "hf://mistralai/Mistral-7B-Instruct-v0.3": causal_infer_template,
-    "oai://gpt-4o-mini": oai_infer_template,
-    "oai://gpt-4-turbo": oai_infer_template,
-    "oai://gpt-3.5-turbo-0125": oai_infer_template,
-    "mistral://open-mistral-7b": oai_infer_template,
-    "llamafile://mistralai/Mistral-7B-Instruct-v0.2": oai_infer_template,
+    "hf://facebook/bart-large-cnn": bart_eval_template,
+    "hf://mikeadimech/longformer-qmsum-meeting-summarization": seq2seq_eval_template,
+    "hf://mrm8488/t5-base-finetuned-summarize-news": seq2seq_eval_template,
+    "hf://Falconsai/text_summarization": seq2seq_eval_template,
+    "hf://mistralai/Mistral-7B-Instruct-v0.3": causal_eval_template,
+    "oai://gpt-4o-mini": oai_eval_template,
+    "oai://gpt-4-turbo": oai_eval_template,
+    "oai://gpt-3.5-turbo-0125": oai_eval_template,
+    "mistral://open-mistral-7b": oai_eval_template,
+    "llamafile://mistralai/Mistral-7B-Instruct-v0.2": oai_eval_template,
 }
 
 

bce050ac64c07dcfb6c919b0f7653fdfdc937683
george-mzai <george@mozilla.ai>
2024-10-22 16:12:38 +0300

    💥 FE dev environment setup

---
+++ frontend/src/router/index.js
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from 'vue-router';
+// import HomePage from '../pages/HomePage.vue';
+
+const routes = [
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;

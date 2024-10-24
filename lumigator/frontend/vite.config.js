b1c5d241d80efd6a34e7f85e784eefa88353a82a
george-mzai <george@mozilla.ai>
2024-10-24 12:42:42 +0300

    clean setup

--- lumigator/frontend/vite.config.js
+++ lumigator/frontend/vite.config.js
@@ -1,14 +1,11 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 import path from 'path';
 
 export default defineConfig({
   plugins: [
-    vue({
-      template: { transformAssetUrls }
-    }),
+    vue(),
   ],
   resolve: {
     alias: {

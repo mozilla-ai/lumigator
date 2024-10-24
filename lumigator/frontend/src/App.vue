e873a3b6eef2dfc459a006ba2a1948ae2209156b
george-mzai <george@mozilla.ai>
2024-10-24 22:27:21 +0300

    code style

--- lumigator/frontend/src/App.vue
+++ lumigator/frontend/src/App.vue
@@ -1,19 +1,16 @@
 <script setup>
- import LDatasets from './components/LDatasets.vue'
-	import http from '@/services/http/index.js';
+import LDatasets from './components/LDatasets.vue';
+import http from '@/services/http/index.js';
 
-
-	const getDatasetDetails = async (id) => {
-		const response = await http.get(`datasets/${id}`);
-  	console.log(response.data);
-	}
+const getDatasetDetails = async (id) => {
+  const response = await http.get(`datasets/${id}`);
+  console.log(response.data);
+};
 </script>
 <template>
   <div>
     <h1 class="logo">🐊 Lumigator</h1>
-		<l-datasets
-			@dataset-selected="getDatasetDetails($event)"
-		/>
+    <l-datasets @dataset-selected="getDatasetDetails($event)" />
   </div>
 </template>
 
@@ -23,7 +20,7 @@
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
-	cursor: default;
+  cursor: default;
 }
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);

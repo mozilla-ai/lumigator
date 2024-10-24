157ea1476a685dd7c27cb51c54aa2a662eba2e5f
george-mzai <george@mozilla.ai>
2024-10-24 11:58:17 +0300

    🧹 clean up

--- lumigator/frontend/src/main.js
+++ lumigator/frontend/src/main.js
@@ -1,14 +1,9 @@
+import '@/styles/app.scss';
 // main.js
 import { createApp } from 'vue';
 import App from './App.vue';
-import { createPinia } from 'pinia';
-import router from './router/index.js';
-import quasar from './quasar'; // Import your Quasar configuration
 
 import 'quasar/src/css/index.sass'; // Import Quasar styles
 
 createApp(App)
-  .use(createPinia())
-  .use(router)
-  .use(quasar)
   .mount('#app');

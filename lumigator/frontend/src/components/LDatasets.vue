04929c0fe0c1cd9ffa0fbcad1b99d2d738fa52ef
george <giosyrianos@gmail.com>
2024-11-04 23:10:43 +0200

    use Intl to format date

--- lumigator/frontend/src/components/LDatasets.vue
+++ lumigator/frontend/src/components/LDatasets.vue
@@ -5,15 +5,16 @@ const props = defineProps({
 const emit = defineEmits(['dataset-selected', 'remove']);
 
 const formatDate = (dateString) => {
-  const date = new Date(dateString);
+	const date = new Date(dateString);
 
-  const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const year = String(date.getFullYear()).slice(-2);
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-
-  return `${day}/${month}/${year} - ${hours}:${minutes}`;
+	return new Intl.DateTimeFormat('en-GB', {
+		day: '2-digit',
+		month: '2-digit',
+		year: '2-digit',
+		hour: '2-digit',
+		minute: '2-digit',
+		hour12: false,
+	}).format(date).replace(',', ' -');
 };
 
 const onDatasetSelect = (id) => {

76bb5b425053f40fe939d6caa10e0dff0747ae0c
george-mzai <giosyrianos@gmail.com>
2024-11-06 09:11:28 -0500

    experiments basic page

---
+++ lumigator/frontend/src/components/pages/LExperiments.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="l-experiments">
+		<h2>Experiments Page</h2>
+		<!-- Add your component content here -->
+	</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+/* Add your styles here */
+.l-experiments {
+	display: grid;
+	place-items: center;
+}
+</style>

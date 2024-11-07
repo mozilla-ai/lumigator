7a0748dcf1a10ebb84c1f5931718c9bfef56fbf3
george-mzai <george@mozilla.ai>
2024-11-07 15:04:25 -0500

    🧹

--- lumigator/frontend/src/services/datasets/datasetsService.js
+++ lumigator/frontend/src/services/datasets/datasetsService.js
@@ -3,7 +3,6 @@ import { PATH_GET_ALL_DATASETS } from './api';
 
 async function fetchDatasets() {
 	const response = await http.get(PATH_GET_ALL_DATASETS())
-	console.log(response.data);
 	return response.data.items;
 }
 

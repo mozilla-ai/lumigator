d9c2495e50154b799c221e0b8c84691b090927a3
Peter Wilson <peter@mozilla.ai>
2024-11-07 14:23:29 -0500

    add missing test

--- lumigator/python/mzai/backend/backend/tests/api/routes/test_health.py
+++ lumigator/python/mzai/backend/backend/tests/api/routes/test_health.py
@@ -32,6 +32,19 @@ def test_get_job_metadata_not_found(app_client: TestClient):
         data = response.json()
         assert data["detail"] == f"Job metadata for ID: {job_id} not found"
 
+def test_get_job_metadata_not_ok(app_client: TestClient):
+    job_id = "22e146e3-10eb-4a55-8018-218829c4752a"
+    with Mocker() as cm:
+        cm.get(
+            url=f"{settings.RAY_DASHBOARD_URL}/api/jobs/{job_id}",
+            status_code=status.HTTP_409_CONFLICT,
+        )
+        response = app_client.get(f"/health/jobs/{job_id}")
+        assert response is not None
+        assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+        data = response.json()
+        assert data["detail"] == f"Unexpected error getting job metadata for ID: {job_id}"
+
 def test_get_job_metadata_ok(app_client: TestClient,
                              json_data_health_job_metadata_ray,
                              json_data_health_job_metadata_ok):

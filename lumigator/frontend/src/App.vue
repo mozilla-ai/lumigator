df1e8820092bbcc432d81c7bf6c4f315b24a571a
george-mzai <george@mozilla.ai>
2024-11-07 14:53:05 -0500

    🧹

--- lumigator/frontend/src/App.vue
+++ lumigator/frontend/src/App.vue
@@ -14,61 +14,14 @@
 	</div>
 </template>
 <script setup>
-import { ref, onMounted } from 'vue';
-import http from '@/services/http/index.js';
 import LMenu from '@/components/LMenu.vue'
 
-const datasets = ref([]);
-
-const getDatasets = async () => {
-  const response = await http.get('datasets/');
-  console.log(response.data.items);
-  datasets.value = response.data.items;
-};
-
-const getDatasetDetails = async (id) => {
-	const response = await http.get(`datasets/${id}`);
-	// this will fetch further information for the selected dataset
-	// when endpoint is ready
-  console.log(response.data);
-};
-
-const deleteDataset = async (id) => {
-  try {
-    const response = await http.delete(`datasets/${id}`);
-
-    if (response.status === 200 || response.status === 204) {
-      setTimeout(() => {
-        getDatasets();
-      }, 1000);
-    } else {
-      console.error("Unexpected response status: ", response.status);
-    }
-  } catch (error) {
-    console.error("Error deleting dataset:", error);
-  }
-};
-
-onMounted(() => {
-  getDatasets();
-});
 </script>
 
 <style scoped lang="scss">
-// .logo {
-//   height: 6em;
-//   padding: 1.5em;
-//   will-change: filter;
-//   transition: filter 300ms;
-//   cursor: default;
-// }
-// .logo:hover {
-//   filter: drop-shadow(0 0 2em #646cffaa);
-// }
-
 #app {
 	height: 100vh;
-  padding: $l-spacing-1;
+	padding: $l-spacing-1;
 	margin: auto;
 	background-color: $l-main-bg;
 	display: grid;
@@ -78,15 +31,13 @@ onMounted(() => {
 	text-align: center;
 
 	.header {
-		// border: 2px solid red;
 		background-color: $l-main-bg;
 		grid-column: 1/-1;
 		grid-row: 1;
 	}
 
-	.l-menu-container{
+	.l-menu-container {
 		background-color: $l-main-bg;
-		// border: 2px solid blue;
 		grid-row: 2;
 	}
 
@@ -94,6 +45,8 @@ onMounted(() => {
 		background-color: $l-card-bg;
 		border-radius: $l-main-radius;
 		grid-row: 2;
+		display: grid;
+		place-items: center;
 	}
 }
 </style>

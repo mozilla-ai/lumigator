41c04a34fed4afe4d360eef4a929c8b3600b4026
george-mzai <george@mozilla.ai>
2024-11-09 13:38:27 -0500

    table component basic implementation

---
+++ lumigator/frontend/src/components/molecules/LTable.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="l-table">
+		<DataTable :value="tableData" size="medium">
+			<Column v-for="(column, idx) in columns" :key="idx" :field="column.field" :header="column.header" />
+		</DataTable>
+	</div>
+</template>
+
+<script setup>
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+
+defineProps({
+	columns: {
+		type: Array,
+		required: true,
+		validator: (value) => {
+			return value.every(item => 'field' in item && 'header' in item);
+		}
+	},
+	tableData: {
+		type: Array,
+		required: true,
+	}
+
+})
+
+
+</script>
+
+<style scoped lang="scss">
+	.l-table {
+		width: 100%;
+		display: flex;
+		place-content: center;
+	}
+</style>

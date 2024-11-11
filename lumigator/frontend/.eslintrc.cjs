c7d2b49930f72eddc718b28d32feac3c30150001
george-mzai <george@mozilla.ai>
2024-11-11 12:04:35 +0200

    code style

--- lumigator/frontend/.eslintrc.cjs
+++ lumigator/frontend/.eslintrc.cjs
@@ -2,21 +2,21 @@
 require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:vue/vue3-recommended',
-    'eslint:recommended',
-    'prettier',
-  ],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: ['vue'],
-  rules: {
-    // Add custom ESLint rules here
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'plugin:vue/vue3-recommended',
+		'eslint:recommended',
+		'prettier',
+	],
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: ['vue'],
+	rules: {
+		// Add custom ESLint rules here
+	},
 };

d6e4b1d3ba48cf8eca6bbc519f0a8d9817e8523d
Kyle White <kylewhite@gmail.com>
2024-11-11 16:21:41 -0500

    Try factoring out localstack client

--- lumigator/python/mzai/backend/backend/tests/conftest.py
+++ lumigator/python/mzai/backend/backend/tests/conftest.py
@@ -7,26 +7,28 @@ import requests_mock
 from botocore.exceptions import ClientError
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
-from mypy_boto3_s3 import S3Client
 from sqlalchemy import Engine, create_engine
 from sqlalchemy.orm import Session
 from testcontainers.localstack import LocalStackContainer
 
-from backend.api.deps import get_db_session, get_s3_client
+from backend.api.deps import get_db_session
 from backend.api.router import API_V1_PREFIX
 from backend.main import create_app
 from backend.settings import settings
 
 # TODO: Break tests into "unit" and "integration" folders based on fixture dependencies
 
+
 def common_resources_dir() -> Path:
     return Path(__file__).parent.parent.parent.parent
+
 
 @pytest.fixture(scope="function")
 def dialog_dataset():
     filename = common_resources_dir() / "sample_data" / "dialogsum_exc.csv"
     with Path(filename).open("rb") as f:
         yield f
+
 
 @pytest.fixture(scope="session", autouse=True)
 def db_engine():
@@ -59,35 +61,6 @@ def localstack_container():
         yield localstack
 
 
-@pytest.fixture(scope="session", autouse=True)
-def setup_aws(localstack_container: LocalStackContainer):
-    """Setup env vars/AWS resources for use with the LocalStack container."""
-    # Initialize S3
-    s3 = localstack_container.get_client("s3")
-    try:
-        s3.create_bucket(
-            Bucket=settings.S3_BUCKET,
-            CreateBucketConfiguration={"LocationConstraint": localstack_container.region_name},
-        )
-    except ClientError as e:
-        if e.response["Error"]["Code"] == "BucketAlreadyOwnedByYou":
-            print("Bucket already created, continue")
-        else:
-            raise e
-    # add ENV vars for FSSPEC access to S3 (s3fs + HuggingFace datasets)
-    os.environ["FSSPEC_S3_KEY"] = "testcontainers-localstack"
-    os.environ["FSSPEC_S3_SECRET"] = "testcontainers-localstack" # pragma: allowlist secret
-    os.environ["FSSPEC_S3_ENDPOINT_URL"] = localstack_container.get_url()
-    os.environ["AWS_ACCESS_KEY_ID"] = "testcontainers-localstack"
-    os.environ["AWS_SECRET_ACCESS_KEY"] = "testcontainers-localstack" # pragma: allowlist secret
-    os.environ["AWS_ENDPOINT_URL"] = localstack_container.get_url()
-
-
-@pytest.fixture(scope="function")
-def s3_client(localstack_container: LocalStackContainer) -> S3Client:
-    return localstack_container.get_client("s3")
-
-
 @pytest.fixture(scope="session")
 def app():
     """Create the FastAPI app bound to DB managed via Alembic.
@@ -103,12 +76,14 @@ def app():
     app = create_app()
     return app
 
+
 @pytest.fixture(scope="function")
 def app_client(app: FastAPI):
     """Create a test client for calling the FastAPI app."""
     base_url = f"http://dev{API_V1_PREFIX}"  # Fake base URL for the app
     with TestClient(app, base_url=base_url) as c:
         yield c
+
 
 @pytest.fixture(scope="function")
 def local_client(app: FastAPI):
@@ -119,7 +94,7 @@ def local_client(app: FastAPI):
 
 
 @pytest.fixture(scope="function", autouse=True)
-def dependency_overrides(app: FastAPI, db_session: Session, s3_client: S3Client) -> None:
+def dependency_overrides(app: FastAPI, db_session: Session) -> None:
     """Override the FastAPI dependency injection for test DB sessions.
 
     Reference: https://fastapi.tiangolo.com/he/advanced/testing-database/
@@ -128,23 +103,23 @@ def dependency_overrides(app: FastAPI, db_session: Session, s3_client: S3Client)
     def get_db_session_override():
         yield db_session
 
-    def get_s3_client_override():
-        yield s3_client
-
     app.dependency_overrides[get_db_session] = get_db_session_override
-    app.dependency_overrides[get_s3_client] = get_s3_client_override
+
 
 @pytest.fixture(scope="session")
 def resources_dir() -> Path:
     return Path(__file__).parent / "data"
 
+
 @pytest.fixture(scope="session")
 def json_data_health_job_metadata_ok(resources_dir) -> Path:
     return resources_dir / "health_job_metadata.json"
 
+
 @pytest.fixture(scope="session")
 def json_data_health_job_metadata_ray(resources_dir) -> Path:
     return resources_dir / "health_job_metadata_ray.json"
+
 
 @pytest.fixture(scope="function")
 def request_mock() -> requests_mock.Mocker:

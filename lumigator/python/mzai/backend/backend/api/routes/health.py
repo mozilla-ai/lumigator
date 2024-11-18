8b9304c322909627e34cd5e2609c882e71b49273
Javier Torres <javier@mozilla.ai>
2024-11-18 11:02:33 +0100

    Review rework

--- lumigator/python/mzai/backend/backend/api/routes/health.py
+++ lumigator/python/mzai/backend/backend/api/routes/health.py
@@ -1,6 +1,6 @@
 import json
-from urllib.parse import urljoin
 from http import HTTPStatus
+from urllib.parse import urljoin
 from uuid import UUID
 
 import loguru
@@ -24,13 +24,18 @@ def get_job_metadata(job_id: UUID) -> JobSubmissionResponse:
     resp = requests.get(urljoin(settings.RAY_JOBS_URL, f"{job_id}"))
 
     if resp.status_code == HTTPStatus.NOT_FOUND:
-        loguru.logger.error(f"Upstream job metadata not found ({resp.status_code}), error:  {resp.text or ''}")
+        loguru.logger.error(
+            f"Upstream job metadata not found ({resp.status_code}), error:  {resp.text or ''}"
+        )
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND,
             detail=f"Job metadata for ID: {job_id} not found",
         )
     elif resp.status_code != HTTPStatus.OK:
-        loguru.logger.error(f"Unexpected status code getting job metadata text: {resp.status_code}, error: {resp.text or ''}")
+        loguru.logger.error(
+            f"Unexpected status code getting job metadata text: "
+             "{resp.status_code}, error: {resp.text or ''}"
+        )
         raise HTTPException(
             status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
             detail=f"Unexpected error getting job metadata for ID: {job_id}",
@@ -52,16 +57,20 @@ def get_job_logs(job_id: UUID) -> JobLogsResponse:
     resp = requests.get(urljoin(settings.RAY_JOBS_URL, f"{job_id}/logs"))
 
     if resp.status_code == HTTPStatus.NOT_FOUND:
-        loguru.logger.error(f"Upstream job logs not found: {resp.status_code}, error: {resp.text or ''}")
+        loguru.logger.error(
+            f"Upstream job logs not found: {resp.status_code}, error: {resp.text or ''}"
+        )
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND,
             detail=f"Job logs for ID: {job_id} not found",
         )
     elif resp.status_code != HTTPStatus.OK:
-        loguru.logger.error(f"Unexpected status code getting job logs: {resp.status_code}, error: {resp.text or ''}")
+        loguru.logger.error(
+            f"Unexpected status code getting job logs: {resp.status_code}, error: {resp.text or ''}"
+        )
         raise HTTPException(
             status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
-            detail=f"Unexpected error getting job logs for ID: {job_id} - {resp.text or ''}",
+            detail=f"Unexpected error getting job logs for ID: {job_id}",
         )
 
     try:
@@ -79,10 +88,12 @@ def get_job_logs(job_id: UUID) -> JobLogsResponse:
 def get_all_jobs() -> list[JobSubmissionResponse]:
     resp = requests.get(settings.RAY_JOBS_URL)
     if resp.status_code != HTTPStatus.OK:
-        loguru.logger.error(f"Unexpected status code getting all jobs: {resp.status_code}, error: {resp.text or ''}")
+        loguru.logger.error(
+            f"Unexpected status code getting all jobs: {resp.status_code}, error: {resp.text or ''}"
+        )
         raise HTTPException(
             status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
-            detail=f"Unexpected error getting job logs - {resp.text or ''}",
+            detail="Unexpected error getting job logs",
         )
     try:
         metadata = json.loads(resp.text)

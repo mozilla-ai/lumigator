364669d2785dd85b28db17c15b938f6299a6d36a
Dimitris Poulopoulos <dimitris@mozilla.ai>
2024-11-20 16:41:00 +0200

    backend: Get supported tasks from models YAML file
    Use the models YAML file to retrieve the list of supported tasks,
    instead of hardcoding it in the code.
    
    Refs #381
    
    Signed-off-by: Dimitris Poulopoulos <dimitris@mozilla.ai>

--- lumigator/python/mzai/backend/backend/api/routes/models.py
+++ lumigator/python/mzai/backend/backend/api/routes/models.py
@@ -4,10 +4,18 @@ import yaml
 from fastapi import APIRouter, HTTPException
 from lumigator_schemas.extras import ListingResponse
 
-SUPPORTED_TASKS = ["summarization"]
 MODELS_PATH = Path(__file__).resolve().parents[2] / "models.yaml"
 
 router = APIRouter()
+
+
+def _get_supported_tasks(data: dict) -> list[str]:
+    tasks = set()
+    for model in data:
+        for task in model.get("tasks", []):
+            tasks.update(task.keys())
+
+    return list(tasks)
 
 
 @router.get("/{task_name}")
@@ -20,15 +28,17 @@ def get_suggested_models(task_name: str) -> ListingResponse[dict]:
     Returns:
         ListingResponse[str]: A list of suggested models.
     """
+    with Path(MODELS_PATH).open() as file:
+        data = yaml.safe_load(file)
+
+    supported_tasks = _get_supported_tasks(data)
+
     # Currently, only summarization task is supported.
     if task_name != "summarization":
         raise HTTPException(
             status_code=400,
-            detail=f"Unsupported task. Choose from: {SUPPORTED_TASKS}",
+            detail=f"Unsupported task. Choose from: {supported_tasks}",
         )
-
-    with Path(MODELS_PATH).open() as file:
-        data = yaml.safe_load(file)
 
     return_data = {
         "total": len(data),

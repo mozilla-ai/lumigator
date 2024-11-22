e29f37aea54bb8e599156897b5c452f429b82abd
george-mzai <george@mozilla.ai>
2024-11-22 15:15:47 +0200

    switch to 'job' endpoint

--- lumigator/frontend/src/services/experiments/api.js
+++ lumigator/frontend/src/services/experiments/api.js
@@ -3,5 +3,5 @@ export const PATH_EXPERIMENT_DETAILS = (experiment_id) => `health/jobs/${experim
 // these 👆 endopoints do not belong to the experiments API,
 // temporary solution until
 // migration to "job" API route
-export const PATH_EXPERIMENT_RESULTS = (job_id) => `experiments/${job_id}/result/download`
-export const PATH_EXPERIMENTS_EVALUATE = () => `experiments/evaluate/`
+export const PATH_EXPERIMENT_RESULTS = (job_id) => `jobs/${job_id}/result/download`
+export const PATH_EXPERIMENTS_EVALUATE = () => `jobs/evaluate/`

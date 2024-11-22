2aad356b70b64eba108a090aa99dfb2126f5544d
Javier Torres <javier@mozilla.ai>
2024-11-22 08:10:15 +0100

    Check git tag before publishing

--- tools/check_version.sh
+++ tools/check_version.sh
@@ -2,7 +2,7 @@
 set -eu
 
 PYVERSION=$(uv run yq -p toml -oy ".project.version" pyproject.toml)
-TAG=$(git describe --exact-match --tags 2> /dev/null)
+TAG=$(git describe --exact-match --tags)
 
 if [ "${PYVERSION}" != "${TAG}" ]; then
     echo "Version mismatch - pyproject:${PYVERSION} git:${TAG}"

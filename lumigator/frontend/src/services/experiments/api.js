4b757d1972958c395647517552cfa784ab859b0b
george-mzai <george@mozilla.ai>
2024-11-22 16:12:56 +0200

    Update lumigator/frontend/src/services/experiments/api.js
    Co-authored-by: Peter Wilson <peter@mozilla.ai>
    Signed-off-by: george-mzai <george@mozilla.ai>

--- lumigator/frontend/src/services/experiments/api.js
+++ lumigator/frontend/src/services/experiments/api.js
@@ -2,5 +2,3 @@ export const PATH_EXPERIMENTS_EVALUATE = () => `jobs/evaluate/`
 export const PATH_EXPERIMENTS_ROOT = () => `health/jobs/`
 export const PATH_EXPERIMENT_DETAILS = (experiment_id) => `health/jobs/${experiment_id}`
 // these 👆 endopoints do not belong to the experiments API,
-// temporary solution until
-// migration to "job" API route

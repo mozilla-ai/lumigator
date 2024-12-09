65d9541c2e1e1dbde5020f6c16685a33b722db1d
Dimitris Poulopoulos <dimitris@mozilla.ai>
2024-12-09 13:10:18 +0200

    inference: Validate Hugging Face inference params
    Validate the values of the parameters used in HF pipelines:
    
    * Check if the model name is a valid HF repo ID
    * Check if the task is a supported task
    * Check if the data type is a valid `torch.dtype` value
    
    Refs #339
    
    Signed-off-by: Dimitris Poulopoulos <dimitris@mozilla.ai>

--- lumigator/python/mzai/jobs/inference/inference_config.py
+++ lumigator/python/mzai/jobs/inference/inference_config.py
@@ -1,9 +1,85 @@
 from enum import Enum
+from typing import Annotated
 
 import torch
+from huggingface_hub.utils import validate_repo_id
 from loguru import logger
-from pydantic import BaseModel, ConfigDict, Field, computed_field
+from pydantic import AfterValidator, BaseModel, BeforeValidator, ConfigDict, Field, computed_field
+from transformers.pipelines import check_task, get_supported_tasks
 from utils import resolve_model_repo
+
+
+def _validate_torch_dtype(x: str | torch.dtype) -> str | torch.dtype:
+    """Validate the given torch dtype.
+
+    If set to "auto", the dtype will be inferred from the Hugging Face model configuration
+    Otherwise, convert the string to a torch.dtype if it is a valid torch dtype
+
+    Args:
+        x (str | torch.dtype): The torch dtype to validate.
+
+    Raises:
+        ValueError: If the given dtype is not a valid torch dtype.
+    """
+    match x:
+        case torch.dtype():
+            return x
+        case str() if x == "auto":
+            return x
+        case str() if hasattr(torch, x) and isinstance(getattr(torch, x), torch.dtype):
+            return getattr(torch, x)
+        case _:
+            raise ValueError(f"{x} is not a valid torch.dtype.")
+
+
+TorchDtype = Annotated[torch.dtype | str, BeforeValidator(lambda x: _validate_torch_dtype(x))]
+
+
+def _validate_model_path(path: str) -> str:
+    """Validate the given model path.
+
+    Resorve the model repo ID and check if it is a valid HuggingFace repo ID.
+
+    Args:
+        path (str): The model path to validate.
+
+    Raises:
+        ValueError: If the path is not a valid HuggingFace repo ID.
+    """
+    raw_path = resolve_model_repo(path)
+
+    if validate_repo_id(raw_path):  # if the path is a valid repo this will return None
+        raise ValueError(f"'{raw_path}' is not a valid HuggingFace repo ID.")
+
+    return path
+
+
+AssetPath = Annotated[str, AfterValidator(lambda x: _validate_model_path(x))]
+
+
+def _validate_task(task: str) -> None:
+    """Validate the given task.
+
+    There are two types of tasks, simple and parameterized tasks
+    For example, "translation" is a simple task but "translation_xx_to_yy" is a parameterized task.
+    Normalize the task to the corresponding simple task and compare it with the supported tasks.
+
+    Args:
+        task (str): The task to validate.
+
+    Raises:
+        ValueError: If the task is not supported.
+    """
+    supported_tasks = get_supported_tasks()
+    normalized_task, _, _ = check_task(task)
+
+    if normalized_task not in supported_tasks:
+        raise ValueError(f"Task '{task}' is not supported. Supported tasks are: {supported_tasks}")
+
+    return task
+
+
+SupportedTask = Annotated[str, AfterValidator(lambda x: _validate_task(x))]
 
 
 class Accelerator(str, Enum):
@@ -43,13 +119,13 @@ class SamplingParameters(BaseModel):
 
 
 class HfPipelineConfig(BaseModel, arbitrary_types_allowed=True):
-    model_path: str = Field(title="The Model HF Hub repo ID", exclude=True)
-    task: str
+    model_path: AssetPath = Field(title="The Model HF Hub repo ID", exclude=True)
+    task: SupportedTask
     revision: str = "main"  # Model version: branch, tag, or commit ID
     use_fast: bool = True  # Whether or not to use a Fast tokenizer if possible
     trust_remote_code: bool = False
-    torch_dtype: str = "auto"
-    accelerator: str = Field(title="Accelerator", default=Accelerator.AUTO, exclude=True)
+    torch_dtype: TorchDtype = "auto"
+    accelerator: Accelerator = Field(title="Accelerator", default=Accelerator.AUTO, exclude=True)
     model_config = ConfigDict(extra="forbid")
 
     @computed_field

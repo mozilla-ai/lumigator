e44d10248142cd0078508764937c257b9de6f3a8
Davide Eynard <davide@mozilla.ai>
2024-12-16 14:53:43 +0100

    Added /experiments_new endpoint
    This commit adds a new route called `experiments_new`. The name is temporary and
    it will be changed to `experiments` after we verify the functionality is the same.
    
    The purpose of this new endpoint is to run an experiment by piping two simpler
    jobs, one specific for inference and the other for evaluation.
    
    At this stage, the code just replicates what is done by `experiments`.

--- lumigator/python/mzai/backend/backend/api/router.py
+++ lumigator/python/mzai/backend/backend/api/router.py
@@ -1,6 +1,14 @@
 from fastapi import APIRouter
 
-from backend.api.routes import completions, datasets, experiments, health, jobs, models
+from backend.api.routes import (
+    completions,
+    datasets,
+    experiments,
+    experiments_new,
+    health,
+    jobs,
+    models,
+)
 from backend.api.tags import Tags
 
 API_V1_PREFIX = "/api/v1"
@@ -12,3 +20,6 @@ api_router.include_router(jobs.router, prefix="/jobs", tags=[Tags.JOBS])
 api_router.include_router(experiments.router, prefix="/experiments", tags=[Tags.EXPERIMENTS])
 api_router.include_router(completions.router, prefix="/completions", tags=[Tags.COMPLETIONS])
 api_router.include_router(models.router, prefix="/models", tags=[Tags.MODELS])
+api_router.include_router(
+    experiments_new.router, prefix="/experiments_new", tags=[Tags.EXPERIMENTS_NEW]
+)

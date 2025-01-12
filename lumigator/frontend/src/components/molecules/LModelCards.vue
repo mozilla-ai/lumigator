37681d7a5a0df022284d098ab53888c84cb013dd
george-mzai <george@mozilla.ai>
2025-01-12 20:44:03 +0200

    select model by clicking the card

--- lumigator/frontend/src/components/molecules/LModelCards.vue
+++ lumigator/frontend/src/components/molecules/LModelCards.vue
@@ -5,12 +5,14 @@
         v-for="model in models"
         :key="model.name"
         class="l-models-list__options-container--option"
+        @click="selectModel(model)"
       >
         <RadioButton
           v-model="selectedModel"
           :inputId="model.uri"
           name="dynamic"
           :value="model"
+          @click.stop
         />
         <label :for="model.uri">{{ model.name }}</label>
       </div>
@@ -20,13 +22,13 @@
 
 <script setup>
 import { ref } from 'vue';
-import { storeToRefs } from 'pinia'
+import { storeToRefs } from 'pinia';
 import { useModelStore } from '@/stores/models/store';
 import RadioButton from 'primevue/radiobutton';
 
 const modelStore = useModelStore();
 const { models } = storeToRefs(modelStore);
-const selectedModel = ref('')
+const selectedModel = ref('');
 
 defineProps({
   modelLink: String
@@ -35,6 +37,10 @@ defineProps({
 defineExpose({
   selectedModel
 })
+
+function selectModel(model) {
+  selectedModel.value = model;
+}
 </script>
 
 <style scoped lang="scss">
@@ -47,18 +53,25 @@ defineExpose({
     padding-top: $l-spacing-1;
     gap: $l-spacing-1;
 
-
     &--option {
       display: grid;
       grid-template-columns: 30px 80% 1fr;
       grid-gap: 5px;
       background-color: $l-grey-300;
+      border: 1px solid $l-gridlines-stroke;
       padding: $l-spacing-1;
       border-radius: $l-main-radius;
+      cursor: pointer;
+      align-items: CENTER;
 
       label {
         font-size: $l-font-size-sm;
+      cursor: pointer;
         color: $l-grey-100;
+      }
+
+      &:hover {
+        border-color: #52525b;
       }
     }
 

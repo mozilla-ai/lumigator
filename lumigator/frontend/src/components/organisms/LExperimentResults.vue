7b1de8bfd120ba9de8cbeb9304e700ccc76c3fa4
george-mzai <george@mozilla.ai>
2025-01-19 17:02:13 +0200

    trigger experiment results drawer

---
+++ lumigator/frontend/src/components/organisms/LExperimentResults.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="experiment-results">
+    <h1>Experiment Results</h1>
+    <pre>{{ selectedExperiment }}</pre>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+import {useExperimentStore} from "@/stores/experiments/store.js";
+import { storeToRefs } from 'pinia';
+
+const experimentStore = useExperimentStore();
+const { selectedExperiment } = storeToRefs(experimentStore);
+
+onMounted(() => {
+  console.log('🧪', selectedExperiment.value)
+})
+</script>
+
+<style scoped lang="scss">
+</style>

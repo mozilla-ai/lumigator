18c06e46be014ea8e46dbb7a26be7870c52a0dd6
Nathan Brake <nathan@mozilla.ai>
2025-01-21 15:45:56 -0500

    Fix unit tests

--- lumigator/python/mzai/backend/backend/tests/unit/services/test_job_service.py
+++ lumigator/python/mzai/backend/backend/tests/unit/services/test_job_service.py
@@ -2,7 +2,6 @@ import pytest
 from lumigator_schemas.jobs import (
     JobInferenceCreate,
 )
-from lumigator_schemas.jobs.jobs import JobType
 
 from backend.services.jobs import JobService
 from backend.settings import settings
@@ -15,7 +14,7 @@ def test_set_null_inference_job_params(job_record, job_service):
         model="hf://facebook/bart-large-cnn",
         dataset="cced289c-f869-4af1-9195-1d58e32d1cc1",
     )
-    params = job_service._get_job_params(JobType.INFERENCE, job_record, request)
+    params = job_service._get_job_params("INFERENCE", job_record, request)
     assert params["max_samples"] == -1
 
 
@@ -27,7 +26,7 @@ def test_set_explicit_inference_job_params(job_record, job_service):
         model="hf://facebook/bart-large-cnn",
         dataset="cced289c-f869-4af1-9195-1d58e32d1cc1",
     )
-    params = job_service._get_job_params(JobType.INFERENCE, job_record, request)
+    params = job_service._get_job_params("INFERENCE", job_record, request)
     assert params["max_samples"] == 10
 
 

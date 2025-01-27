774d13d4d8315ccaede22f5992d29af18fe43ba4
Peter Wilson <peter@mozilla.ai>
2025-01-27 10:33:22 +0000

    Backend: Decouple completions service from HTTP presentation layer (#739)
    * Add upstream error for completions, remove unused methods for listing models, configure mappings
    
    * improved exception handling in service

--- lumigator/python/mzai/backend/backend/main.py
+++ lumigator/python/mzai/backend/backend/main.py
@@ -13,6 +13,7 @@ from starlette.requests import Request
 from starlette.responses import JSONResponse, Response
 
 from backend.api.router import api_router
+from backend.api.routes.completions import completion_exception_mappings
 from backend.api.routes.datasets import dataset_exception_mappings
 from backend.api.tags import TAGS_METADATA
 from backend.services.exceptions.base_exceptions import ServiceError
@@ -90,8 +91,8 @@ def create_app() -> FastAPI:
 
     # Group mappings of service error types to HTTP status code, for routes.
     exception_mappings = [
-        # TODO: Add completions
-        dataset_exception_mappings()  # Datasets
+        completion_exception_mappings(),  # Completions
+        dataset_exception_mappings(),  # Datasets
         # TODO: Add experiments
         # TODO: Add jobs
     ]

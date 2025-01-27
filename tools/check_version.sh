0ae29020e5c1e4aff71baebc2b728ba8db1b87e2
Mario Cariñana <macaab26@gmail.com>
2025-01-27 19:49:14 +0100

    Deleting extra character

--- tools/check_version.sh
+++ tools/check_version.sh
@@ -4,7 +4,7 @@ set -eu
 PYVERSION=$(yq -p toml -oy ".project.version" pyproject.toml)
 TAG=$(git describe --exact-match --tags)
 
-if [ "v${PYVERSION}" != "${TAG}" ]; then
+if [ "${PYVERSION}" != "${TAG}" ]; then
     echo "Version mismatch - pyproject:${PYVERSION} git:${TAG}"
     exit 1
 fi

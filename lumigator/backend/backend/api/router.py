f4c403df8756125b5e84b022f5d70ca65cc36b0f
Nathan Brake <33383515+njbrake@users.noreply.github.com>
2025-02-06 10:56:21 -0500

    Remove the unused "completions" route (#819)
    The completions route is unused and is a duplicate of the functionality of the inference job.

--- lumigator/backend/backend/api/router.py
+++ lumigator/backend/backend/api/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from backend.api.routes import completions, datasets, experiments, health, jobs, models, workflows
+from backend.api.routes import datasets, experiments, health, jobs, models, workflows
 from backend.api.tags import Tags
 
 API_V1_PREFIX = "/api/v1"
@@ -10,7 +10,6 @@ api_router.include_router(health.router, prefix="/health", tags=[Tags.HEALTH])
 api_router.include_router(datasets.router, prefix="/datasets", tags=[Tags.DATASETS])
 api_router.include_router(jobs.router, prefix="/jobs", tags=[Tags.JOBS])
 api_router.include_router(experiments.router, prefix="/experiments", tags=[Tags.EXPERIMENTS])
-api_router.include_router(completions.router, prefix="/completions", tags=[Tags.COMPLETIONS])
 api_router.include_router(models.router, prefix="/models", tags=[Tags.MODELS])
 api_router.include_router(
     workflows.router, prefix="/workflows", tags=[Tags.WORKFLOWS], include_in_schema=False

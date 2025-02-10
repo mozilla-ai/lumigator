b75edfc339be173af9ff5f39786de67a8258bf23
Nathan Brake <nathan@mozilla.ai>
2025-02-10 12:07:50 -0500

    delete the evaluator

--- lumigator/backend/backend/config_templates.py
+++ lumigator/backend/backend/config_templates.py
@@ -1,73 +1,6 @@
 # Evaluation templates
 from lumigator_schemas.jobs import JobType
 
-seq2seq_eval_template = """{{
-    "name": "{job_name}/{job_id}",
-    "model": {{ "path": "{model_uri}" }},
-    "dataset": {{ "path": "{dataset_path}" }},
-    "evaluation": {{
-        "metrics": ["rouge", "meteor", "bertscore"],
-        "use_pipeline": true,
-        "max_samples": {max_samples},
-        "return_input_data": true,
-        "return_predictions": true,
-        "storage_path": "{storage_path}",
-        "skip_inference": "{skip_inference}"
-    }}
-}}"""
-
-bart_eval_template = """{{
-    "name": "{job_name}/{job_id}",
-    "model": {{ "path": "{model_uri}" }},
-    "tokenizer": {{ "path": "{model_uri}", "mod_max_length": 1024 }},
-    "dataset": {{ "path": "{dataset_path}" }},
-    "evaluation": {{
-        "metrics": ["rouge", "meteor", "bertscore"],
-        "use_pipeline": true,
-        "max_samples": {max_samples},
-        "return_input_data": true,
-        "return_predictions": true,
-        "storage_path": "{storage_path}",
-        "skip_inference": "{skip_inference}"
-    }}
-}}"""
-
-causal_eval_template = """{{
-    "name": "{job_name}/{job_id}",
-    "model": {{ "path": "{model_uri}" }},
-    "dataset": {{ "path": "{dataset_path}" }},
-    "evaluation": {{
-        "metrics": ["rouge", "meteor", "bertscore"],
-        "use_pipeline": false,
-        "max_samples": {max_samples},
-        "return_input_data": true,
-        "return_predictions": true,
-        "storage_path": "{storage_path}",
-        "skip_inference": "{skip_inference}"
-    }}
-}}"""
-
-oai_eval_template = """{{
-    "name": "{job_name}/{job_id}",
-    "model": {{
-        "inference": {{
-            "base_url": "{model_url}",
-            "engine": "{model_uri}",
-            "system_prompt": "{system_prompt}",
-            "max_retries": 3
-        }}
-    }},
-    "dataset": {{ "path": "{dataset_path}" }},
-    "evaluation": {{
-        "metrics": ["rouge", "meteor", "bertscore"],
-        "max_samples": {max_samples},
-        "return_input_data": true,
-        "return_predictions": true,
-        "storage_path": "{storage_path}",
-        "skip_inference": "{skip_inference}"
-    }}
-}}"""
-
 # TODO: this default evaluation template should serve for most purposes
 #       after we deprecate evaluator, as it won't include predictions (model
 #       name is just passed for reference and not for actual inference).
@@ -169,19 +102,9 @@ templates = {
         "default": default_infer_template,
         "hf://facebook/bart-large-cnn": bart_infer_template,
         "oai://gpt-4o-mini": oai_infer_template,
-        "oai://gpt-4o": oai_eval_template,
+        "oai://gpt-4o": oai_infer_template,
         "mistral://open-mistral-7b": oai_infer_template,
         "llamafile://mistralai/Mistral-7B-Instruct-v0.2": oai_infer_template,
-    },
-    JobType.EVALUATION: {
-        "default": causal_eval_template,
-        "hf://facebook/bart-large-cnn": bart_eval_template,
-        "hf://Falconsai/text_summarization": seq2seq_eval_template,
-        "hf://mistralai/Mistral-7B-Instruct-v0.3": causal_eval_template,
-        "oai://gpt-4o-mini": oai_eval_template,
-        "oai://gpt-4o": oai_eval_template,
-        "mistral://open-mistral-7b": oai_eval_template,
-        "llamafile://mistralai/Mistral-7B-Instruct-v0.2": oai_eval_template,
     },
     # TODO: Remove the old EVALUATION section and rename EVALUATION_LITE
     #       to EVALUATION after we deprecate evaluator. Also remove the

c73a5c900fc3e318f47e3fd3bf5056e05cea1f40
Javier Torres <javier@mozilla.ai>
2025-02-17 13:42:24 +0100

    Initial job interface, refactor in workflow status

--- lumigator/backend/backend/tests/integration/api/routes/test_api_workflows.py
+++ lumigator/backend/backend/tests/integration/api/routes/test_api_workflows.py
@@ -38,7 +38,7 @@ def test_health_ok(local_client: TestClient):
     assert response.status_code == 200
 
 
-def test_upload_data_launch_job(
+def _test_upload_data_launch_job(
     local_client: TestClient,
     dialog_dataset,
     dependency_overrides_services,
@@ -141,7 +141,7 @@ def test_upload_data_launch_job(
 
 
 @pytest.mark.parametrize("unnanotated_dataset", ["dialog_empty_gt_dataset", "dialog_no_gt_dataset"])
-def test_upload_data_no_gt_launch_annotation(
+def _test_upload_data_no_gt_launch_annotation(
     request: pytest.FixtureRequest,
     local_client: TestClient,
     unnanotated_dataset,
@@ -382,9 +382,12 @@ def test_timedout_experiment(local_client: TestClient, dialog_dataset, dependenc
 
 def test_experiment_non_existing(local_client: TestClient, dependency_overrides_services):
     non_existing_id = "d34dbeef-4bea-4d19-ad06-214202165812"
-    response = local_client.get(f"/experiments/new/{non_existing_id}")
+    response = local_client.get(f"/experiments/{non_existing_id}")
     assert response.status_code == 404
-    assert response.json()["detail"] == "Not Found"
+    assert (
+        response.json()["detail"]
+        == f"Experiment with ID {non_existing_id} not found: Experiment ID {non_existing_id} not found in mlflow"
+    )
 
 
 def test_job_non_existing(local_client: TestClient, dependency_overrides_services):

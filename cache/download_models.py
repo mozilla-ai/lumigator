535d4eb43bb1b65f344d276638be97ff35c2626c
Alejandro Gonzalez <agpituk@gmail.com>
2025-02-19 16:04:51 +0100

    Fix some comments

--- cache/download_models.py
+++ cache/download_models.py
@@ -2,7 +2,7 @@ import os
 
 from huggingface_hub import snapshot_download
 
-# Get the HF_MODELS environment variable (default if not provided)
+# Get the MODELS_CACHED environment variable (defaults to bart if not provided)
 models_env = os.environ.get("MODELS_CACHED", "facebook/bart-large-cnn")
 # Split by comma and strip whitespace
 models = [model.strip() for model in models_env.split(",")]

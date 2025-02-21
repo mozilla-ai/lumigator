cca2f990be19bd87aad53478b233989d9f9ae5e7
Davide Eynard <davide.eynard@gmail.com>
2025-02-21 15:10:47 +0000

    Fixes to enable model preloading
    - updated model name to `roberta-large` in config (the FacebookAI one
      is the same but bertscore reads the one with no company name from cache)
    - fixed Dockerfile and docker-compose to run downloads in container and
      not at image-creation time
    - updated download_models.py to provide meaningful updates to the users

--- cache/download_models.py
+++ cache/download_models.py
@@ -1,12 +1,18 @@
 import os
 
 from huggingface_hub import snapshot_download
+from loguru import logger
+
+logger.info("*** Inference models caching ***")
 
 # Get the MODELS_CACHED environment variable (defaults to bart if not provided)
 models_env = os.environ.get("MODELS_CACHED", "facebook/bart-large-cnn")
+logger.info(f"Caching the following models: {models_env}")
+logger.info("(this might take a while...)")
+
 # Split by comma and strip whitespace
 models = [model.strip() for model in models_env.split(",")]
 
-for model in models:
+for i, model in enumerate(models):
     model_path = snapshot_download(model, cache_dir="/home/ray/.cache/huggingface/hub")
-    print(f"Model {model} downloaded")
+    logger.info(f"Model {i + 1}/{len(models)} downloaded: {model}")

73496f1be4d890c5529b7f6ffa248479944a28d8
Khaled Osman <khaled.osmaan@gmail.com>
2025-02-25 12:53:37 +0100

    cleanup

--- lumigator/frontend/e2e/datasets.spec.ts
+++ lumigator/frontend/e2e/datasets.spec.ts
@@ -9,19 +9,11 @@ const sampleDatasetFilePath = path.resolve(currentDir, '../../sample_data/dialog
 test('successfully uploads a dataset', async ({ page }) => {
   await page.goto('/');
 
+  // find & click the provide dataset button and capture the system file chooser dialog
   const fileChooserPromise = page.waitForEvent('filechooser');
-  // find & click the provide dataset button
   const button = await page.getByRole('button', { name: 'Provide Dataset' });
   await button.click();
-
   const fileChooser = await fileChooserPromise;
-
-  // validate that the sample dataset file exists in the expected directory
-  console.log('Resolved File Path:', sampleDatasetFilePath);
-  if (!fs.existsSync(sampleDatasetFilePath)) {
-    console.log('Error: File does not exist at', sampleDatasetFilePath);
-    return;
-  }
 
   // choose the sample dataset file from the file chooser dialog
   await fileChooser.setFiles(sampleDatasetFilePath);
@@ -30,7 +22,7 @@ test('successfully uploads a dataset', async ({ page }) => {
   const uploadButton = await page.getByRole('button', { name: 'Upload' });
   await uploadButton.click();
 
-  // wait for the api requests to upload the file and refetch the datasets to finish
+  // wait for the api requests to upload the file and refetch the datasets
   const [response] = await Promise.all([
     page.waitForResponse(response => response.url().includes('datasets') && response.status() === 201 && response.request().method() === 'POST'),
   page.waitForRequest(request => request.url().includes('datasets') && request.method() === 'GET')
@@ -39,6 +31,6 @@ test('successfully uploads a dataset', async ({ page }) => {
  // get the returned filename and id from the api response
   const { id, filename } = await response.json();
 
-  // wait for the new table row with the dataset id and filename to be rendered, (ids are shortened so we only check the first 20 characters)
+  // wait for the new table row with the new dataset id and filename to be rendered, (ids are shortened so we only check the first 20 characters)
   await expect(page.locator(`table tr:has(td:text("${id.slice(0, 20)}")):has(td:text("${filename}"))`)).toBeVisible();
 })

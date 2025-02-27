143708379a951bbdb73a8ddf18ccf247d872d9de
javiermtorres <javier@mozilla.ai>
2025-02-27 15:40:48 +0100

    Make a clean interface for jobs (#939)
    * Initial job interface, refactor in workflow status
    
    * Review rework
    
    * Fix merge issues
    
    * Align with main mlflow implementation
    
    * Initial job interface, refactor in workflow status
    
    * Review rework
    
    * Attempt to make backend code available from the job implementation
    
    * Attempt to make backend code available from the job implementation
    
    * Tweak paths to allow job importing in backend
    
    * Align with main
    
    * Fix task checks
    
    * Review rework
    
    * Align with main
    
    * Fix job definition fixture

--- lumigator/backend/backend/tests/integration/api/routes/test_api_workflows.py
+++ lumigator/backend/backend/tests/integration/api/routes/test_api_workflows.py
@@ -382,9 +382,9 @@ def test_timedout_experiment(local_client: TestClient, dialog_dataset, dependenc
 
 def test_experiment_non_existing(local_client: TestClient, dependency_overrides_services):
     non_existing_id = "d34dbeef-4bea-4d19-ad06-214202165812"
-    response = local_client.get(f"/experiments/new/{non_existing_id}")
+    response = local_client.get(f"/experiments/{non_existing_id}")
     assert response.status_code == 404
-    assert response.json()["detail"] == "Not Found"
+    assert response.json()["detail"] == f"Experiment with ID {non_existing_id} not found"
 
 
 def test_job_non_existing(local_client: TestClient, dependency_overrides_services):

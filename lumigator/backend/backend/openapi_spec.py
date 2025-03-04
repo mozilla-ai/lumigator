4684e8769e806938bad1f033fa86f8e9ac8d9391
Javier Torres <javier@mozilla.ai>
2025-03-04 17:31:56 +0100

    Generate openapi offline

--- lumigator/backend/backend/openapi_spec.py
+++ lumigator/backend/backend/openapi_spec.py
@@ -1,18 +1,29 @@
 import json
 from pathlib import Path
+import argparse
 
 from fastapi.openapi.utils import get_openapi
 
 from backend.main import app
 
-openapi_path = Path('../../docs/source/specs/openapi.json')
-with openapi_path.open('w') as f:
-    json.dump(get_openapi(
-        title=app.title,
-        version=app.version,
-        openapi_version=app.openapi_version,
-        description=app.description,
-        routes=app.routes,
-        # openapi_prefix=app.openapi_prefix,
-    ), f)
+def dump_openapi(dst: str) -> None:
+    openapi_path = Path(dst)
+    with openapi_path.open('w') as f:
+        json.dump(get_openapi(
+            title=app.title,
+            version=app.version,
+            openapi_version=app.openapi_version,
+            description=app.description,
+            routes=app.routes,
+            # openapi_prefix=app.openapi_prefix,
+        ), f)
 
+def build_argparser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("target_path", help="path to store the openapi spec (overwrites contents if present)")
+    return parser
+
+if __name__ == '__main__':
+    parser = build_argparser()
+    args = parser.parse_args()
+    dump_openapi(args.target_path)

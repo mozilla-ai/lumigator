e883679a20780aa06461b6fd8a8fe1f336981552
Khaled Osman <khaled.osmaan@gmail.com>
2025-03-06 15:46:58 +0100

    feat: expandable rows

--- lumigator/frontend/src/components/common/DatasetViewer.vue
+++ lumigator/frontend/src/components/common/DatasetViewer.vue
@@ -15,92 +15,26 @@
         @click="handleDownloadClicked"
       ></PrimeVueButton>
     </template>
-    <DataTable
-      class="gridlines"
-      :value="data"
-      :reorderableColumns="true"
-      ref="dataTable"
-      :removableSort="true"
-      :scrollable="true"
-      scrollHeight="flex"
-      :resizableColumns="false"
-      :columnResizeMode="'fit'"
-      :showGridlines="true"
-      :stripedRows="false"
-      :exportFilename="downloadFileName"
-      :globalFilterFields="columns"
-      v-model:filters="filters"
-      :editMode="isEditable ? 'cell' : undefined"
-      @cell-edit-complete="onCellEditComplete"
-      @cell-edit-cancel="onCellEditCancel"
-    >
-      <template #header>
-        <div style="display: flex; gap: 2rem; justify-content: space-between">
-          <MultiSelect
-            :modelValue="selectedColumns"
-            :options="columns"
-            @update:modelValue="onToggle"
-            display="chip"
-            placeholder="Select Columns"
-          >
-          </MultiSelect>
-
-          <IconField v-if="isSearchEnabled">
-            <InputIcon>
-              <i class="pi pi-search"></i>
-            </InputIcon>
-            <InputText v-model="filters['global'].value" placeholder="Keyword Search" />
-          </IconField>
-        </div>
-      </template>
-      <template #empty> No items found. </template>
-      <Column v-if="showRowNumber" key="rowNumber" field="rowNumber" header="" sortable></Column>
-      <Column
-        v-for="col in selectedColumns"
-        sortable
-        :key="col"
-        :field="col"
-        :header="col"
-        :style="`width: ${(1 / selectedColumns.length) * 100}%`"
-      >
-        <template #editor="{ data, field }">
-          <PrimeVueTextarea v-model="data[field]" autoResize autofocus fluid></PrimeVueTextarea>
-        </template>
-      </Column>
-    </DataTable>
+    <TableView :data="data" :columns="columns" :downloadFileName="downloadFileName" :isEditable="isEditable" :showRowNumber="showRowNumber" :isSearchEnabled="isSearchEnabled" ref="dataTable" />
   </Drawer>
 </template>
 
 <script lang="ts">
 import {
   Button,
-  Column,
-  DataTable,
   Drawer,
-  IconField,
-  InputIcon,
-  InputText,
-  MultiSelect,
-  Textarea,
-  type DataTableCellEditCancelEvent,
-  type DataTableCellEditCompleteEvent,
   type DataTableProps,
 } from 'primevue'
 import { FilterMatchMode } from '@primevue/core/api'
 import { defineComponent, ref, type PropType } from 'vue'
+import TableView from './TableView.vue'
 
 export default defineComponent({
   name: 'DatasetViewer',
   components: {
-    DataTable,
+    TableView,
     Drawer,
-    Column,
     PrimeVueButton: Button,
-    PrimeVueTextarea: Textarea,
-    IconField,
-    InputIcon,
-    InputText,
-    MultiSelect,
   },
   emits: ['close'],
   props: {
@@ -132,38 +66,13 @@ export default defineComponent({
   setup(props) {
     const isVisible = ref(true)
     const dataTable = ref()
-    const selectedColumns = ref(props.columns)
-    const filters = ref({
-      global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-    })
     const handleDownloadClicked = () => {
       dataTable.value.exportCSV()
     }
-
-    const onCellEditComplete = (event: DataTableCellEditCompleteEvent) => {
-      const { data, newValue, field } = event
-
-      data[field] = newValue
-    }
-
-    const onCellEditCancel = (event: DataTableCellEditCancelEvent) => {
-      // prevent esc key from closing the whole modal
-      event.originalEvent.stopPropagation()
-    }
-
-    const onToggle = (selected: string[]) => {
-      selectedColumns.value = props.columns.filter((col) => selected.includes(col))
-    }
-
     return {
       isVisible,
-      dataTable,
       handleDownloadClicked,
-      onCellEditComplete,
-      onCellEditCancel,
-      filters,
-      selectedColumns,
-      onToggle,
+      dataTable,
       ...props,
     }
   },
@@ -172,12 +81,6 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 @use '@/styles/mixins';
-
-/* make the sort icon smaller */
-::v-deep(.p-datatable-sort-icon) {
-  width: 10px;
-  height: 10px;
-}
 
 .title-slot {
   @include mixins.paragraph;
@@ -188,12 +91,5 @@ export default defineComponent({
   margin-left: auto;
   margin-right: 1.5rem;
   gap: 0.125rem;
-}
-
-// global css overrides the cursor to be pointer, reset it back
-:deep(.p-datatable-table-container) {
-  [class*='p-row-'] {
-    cursor: unset;
-  }
 }
 </style>

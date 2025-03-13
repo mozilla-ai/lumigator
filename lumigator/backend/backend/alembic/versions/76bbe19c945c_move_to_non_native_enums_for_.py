38894c1813994bfc92595b4336f25905678c045d
Javier Torres <javier@mozilla.ai>
2025-03-13 11:01:57 +0100

    Switch datasetformat to non native enum

---
+++ lumigator/backend/backend/alembic/versions/76bbe19c945c_move_to_non_native_enums_for_.py
@@ -0,0 +1,43 @@
+"""Move to non-native enums for datasetFormat
+
+Revision ID: 76bbe19c945c
+Revises: da3992fd274e
+Create Date: 2025-03-13 09:52:33.090037
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '76bbe19c945c' # pragma: allowlist secret
+down_revision: Union[str, None] = 'da3992fd274e' # pragma: allowlist secret
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    # Leave removing the job status for next migration
+    with op.batch_alter_table("datasets") as batch_op:
+        native_enum = sa.Enum("JOB", name="datasetformat")
+        non_native_enum = sa.Enum("JOB", "EXPERIMENT", native_enum=False)
+        batch_op.alter_column(
+            "format",
+            type_=non_native_enum,
+            existing_nullable=False,
+            existing_type=native_enum,
+        )
+
+
+def downgrade() -> None:
+    with op.batch_alter_table("jobs") as batch_op:
+        native_enum = sa.Enum("JOB", name="datasetformat")
+        non_native_enum = sa.Enum("JOB", "EXPERIMENT")
+        batch_op.alter_column(
+            "format",
+            type_=native_enum,
+            existing_nullable=False,
+            existing_type=non_native_enum,
+        )

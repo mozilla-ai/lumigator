c417678aab6ceac74aea93beff338c619cb5b4a7
Peter Wilson <peter@mozilla.ai>
2025-03-14 18:03:48 +0000

    Update utils merge dict and tests

--- lumigator/schemas/lumigator_schemas/tests/unit/test_utils_model_operations.py
+++ lumigator/schemas/lumigator_schemas/tests/unit/test_utils_model_operations.py
@@ -12,7 +12,7 @@ class TestModel(BaseModel):
 
 
 @pytest.mark.parametrize(
-    "base_model, overlay_model, deep_merge, expected_merged, expected_overwritten",
+    "base_model, overlay_model, deep_merge, expected_merged, expected_overwritten, expected_skipped",
     [
         # Case 1: base_model is None, overlay_model is provided
         (
@@ -20,6 +20,7 @@ class TestModel(BaseModel):
             TestModel(name="overlay", metadata={"key": "value"}),
             False,
             TestModel(name="overlay", metadata={"key": "value"}),
+            set(),
             set(),
         ),
         # Case 2: overlay_model is None, base_model is provided
@@ -29,6 +30,7 @@ class TestModel(BaseModel):
             False,
             TestModel(name="base", metadata={"key": "base_value"}),
             set(),
+            set(),
         ),
         # Case 3: Base and Overlay models are provided, shallow merge (no deep merge)
         (
@@ -37,6 +39,7 @@ class TestModel(BaseModel):
             False,
             TestModel(name="overlay", metadata={"key": "overlay_value"}),
             {"name", "metadata"},
+            set(),
         ),
         # Case 4: Base and Overlay models are provided, shallow merge (no deep merge)
         (
@@ -45,80 +48,166 @@ class TestModel(BaseModel):
             False,
             TestModel(name="overlay", metadata={"key": "base_value"}),
             {"name"},
+            {"metadata"},
         ),
         # Case 5: Base and Overlay models are provided, deep merge (nested dictionaries)
         (
-            TestModel(name="base", metadata={"key": {"subkey": "base_value"}}),
-            TestModel(name="overlay", metadata={"key": {"subkey": "overlay_value"}}),
+            TestModel(
+                name="base",
+                metadata={
+                    "key": {"subkey": "base_value", "subkey2": "base_value2", "subkey3": "base_value3"},
+                },
+            ),
+            TestModel(name="overlay", metadata={"key": {"subkey": "overlay_value", "subkey2": "base_value2"}}),
             True,
-            TestModel(name="overlay", metadata={"key": {"subkey": "overlay_value"}}),
+            TestModel(
+                name="overlay",
+                metadata={
+                    "key": {"subkey": "overlay_value", "subkey2": "base_value2", "subkey3": "base_value3"},
+                },
+            ),
             {"name", "metadata.key.subkey"},
+            {"metadata.key.subkey2", "metadata.key.subkey3"},
         ),
     ],
 )
-def test_merge_models(base_model, overlay_model, deep_merge, expected_merged, expected_overwritten):
-    merged_model, overwritten_keys = merge_models(base_model, overlay_model, deep_merge)
+def test_merge_models(base_model, overlay_model, deep_merge, expected_merged, expected_overwritten, expected_skipped):
+    merged_model, overwritten_keys, skipped_keys = merge_models(base_model, overlay_model, deep_merge)
 
     assert merged_model == expected_merged
     assert overwritten_keys == expected_overwritten
+    assert skipped_keys == expected_skipped
 
 
 @pytest.mark.parametrize(
-    "base_dict, overlay_dict, deep_merge, expected_merged, expected_overwritten",
-    [
-        # Case 1: Shallow merge (no recursion)
-        ({"key": "base_value"}, {"key": "overlay_value"}, False, {"key": "overlay_value"}, {"key"}),
-        # Case 2: Deep merge (nested dictionaries)
-        (
-            {"key": {"subkey": "base_value"}},
-            {"key": {"subkey": "overlay_value"}},
-            True,
-            {"key": {"subkey": "overlay_value"}},
-            {"key.subkey"},
-        ),
-        # Case 3: Overwriting a top-level key (non-nested)
-        ({"key": "base_value"}, {"key": "overlay_value"}, False, {"key": "overlay_value"}, {"key"}),
-        # Case 4: Overwriting a nested key (deep merge)
-        (
-            {"key": {"subkey": "base_value"}},
-            {"key": {"subkey": "overlay_value"}},
-            True,
-            {"key": {"subkey": "overlay_value"}},
-            {"key.subkey"},
-        ),
-        # Case 5: No overlay values, so base_dict remains the same
-        ({"key": "base_value"}, {}, False, {"key": "base_value"}, set()),
-    ],
-)
-def test_deep_merge_dicts(base_dict, overlay_dict, deep_merge, expected_merged, expected_overwritten):
-    merged_dict, overwritten_keys = deep_merge_dicts(base_dict, overlay_dict, deep_merge)
-
-    assert merged_dict == expected_merged
-    assert overwritten_keys == expected_overwritten
-
-
-@pytest.mark.parametrize(
-    "a, b, expected_overwritten_keys",
+    "a, b, expected_overwritten, expected_skipped",
     [
         (
             "json_workflow_results",
             "json_workflow_results_overlay_all",
-            {"parameters", "metrics", "artifacts"},
+            {
+                "artifacts.predictions",
+                "metrics.bertscore.hashcode",
+                "metrics.bertscore.precision",
+                "metrics.bertscore.recall",
+                "metrics.meteor.meteor",
+                "parameters.name",
+            },
+            {
+                "artifacts.evaluation_time",
+                "artifacts.ground_truth",
+                "metrics.bertscore.f1",
+                "metrics.bertscore.f1_mean",
+                "metrics.bertscore.precision_mean",
+                "metrics.bertscore.recall_mean",
+                "metrics.bleu.bleu",
+                "metrics.bleu.bleu_mean",
+                "metrics.g_eval_summarization",
+                "metrics.meteor.meteor_mean",
+                "metrics.rouge.rouge1",
+                "metrics.rouge.rouge1_mean",
+                "metrics.rouge.rouge2",
+                "metrics.rouge.rouge2_mean",
+                "metrics.rouge.rougeL",
+                "metrics.rouge.rougeL_mean",
+                "metrics.rouge.rougeLsum",
+                "metrics.rouge.rougeLsum_mean",
+                "metrics.token_length",
+                "parameters.dataset.path",
+                "parameters.evaluation.max_samples",
+                "parameters.evaluation.metrics",
+                "parameters.evaluation.return_input_data",
+                "parameters.evaluation.return_predictions",
+                "parameters.evaluation.storage_path",
+            },
         ),
         (
             "json_workflow_results",
             "json_workflow_results_overlay_artifacts_same",
-            {"parameters", "metrics"},
+            {
+                "metrics.bertscore.hashcode",
+                "metrics.bertscore.precision",
+                "metrics.bertscore.recall",
+                "metrics.meteor.meteor",
+                "parameters.name",
+            },
+            {
+                "artifacts.evaluation_time",
+                "artifacts.ground_truth",
+                "artifacts.predictions",
+                "metrics.bertscore.f1",
+                "metrics.bertscore.f1_mean",
+                "metrics.bertscore.precision_mean",
+                "metrics.bertscore.recall_mean",
+                "metrics.bleu.bleu",
+                "metrics.bleu.bleu_mean",
+                "metrics.g_eval_summarization",
+                "metrics.meteor.meteor_mean",
+                "metrics.rouge.rouge1",
+                "metrics.rouge.rouge1_mean",
+                "metrics.rouge.rouge2",
+                "metrics.rouge.rouge2_mean",
+                "metrics.rouge.rougeL",
+                "metrics.rouge.rougeL_mean",
+                "metrics.rouge.rougeLsum",
+                "metrics.rouge.rougeLsum_mean",
+                "metrics.token_length",
+                "parameters.dataset.path",
+                "parameters.evaluation.max_samples",
+                "parameters.evaluation.metrics",
+                "parameters.evaluation.return_input_data",
+                "parameters.evaluation.return_predictions",
+                "parameters.evaluation.storage_path",
+            },
+        ),
+        (
+            "json_workflow_results",
+            "json_workflow_results_overlay_artifacts_missing",
+            {
+                "metrics.bertscore.hashcode",
+                "metrics.bertscore.precision",
+                "metrics.bertscore.recall",
+                "metrics.meteor.meteor",
+                "parameters.name",
+            },
+            {
+                "artifacts.evaluation_time",
+                "artifacts.ground_truth",
+                "artifacts.predictions",
+                "metrics.bertscore.f1",
+                "metrics.bertscore.f1_mean",
+                "metrics.bertscore.precision_mean",
+                "metrics.bertscore.recall_mean",
+                "metrics.bleu.bleu",
+                "metrics.bleu.bleu_mean",
+                "metrics.g_eval_summarization",
+                "metrics.meteor.meteor_mean",
+                "metrics.rouge.rouge1",
+                "metrics.rouge.rouge1_mean",
+                "metrics.rouge.rouge2",
+                "metrics.rouge.rouge2_mean",
+                "metrics.rouge.rougeL",
+                "metrics.rouge.rougeL_mean",
+                "metrics.rouge.rougeLsum",
+                "metrics.rouge.rougeLsum_mean",
+                "metrics.token_length",
+                "parameters.dataset.path",
+                "parameters.evaluation.max_samples",
+                "parameters.evaluation.metrics",
+                "parameters.evaluation.return_input_data",
+                "parameters.evaluation.return_predictions",
+                "parameters.evaluation.storage_path",
+            },
         ),
     ],
 )
-def test_merge_job_results(a, b, expected_overwritten_keys, request):
+def test_merge_job_results(a, b, expected_overwritten, expected_skipped, request):
     # Create JobResultObjects from the JSON data
     job_result_1 = JobResultObject(**request.getfixturevalue(a))
     job_result_2 = JobResultObject(**request.getfixturevalue(b))
 
     # Merge the models using your merge_models function
-    merged_model, overwritten_keys = merge_models(job_result_1, job_result_2, deep_merge=False)
+    merged_model, overwritten_keys, skipped_keys = merge_models(job_result_1, job_result_2, deep_merge=True)
 
     # Assert the merged model is as expected
     bertscore = merged_model.metrics.get("bertscore", {})
@@ -136,4 +225,64 @@ def test_merge_job_results(a, b, expected_overwritten_keys, request):
     assert meteor_meteor[0] == 0.555
 
     # Assert the overwritten keys are as expected
-    assert overwritten_keys == expected_overwritten_keys
+    assert overwritten_keys == expected_overwritten
+    assert skipped_keys == expected_skipped
+
+
+@pytest.mark.parametrize(
+    "base_dict, overlay_dict, deep_merge, expected_merged, expected_overwritten, expected_skipped",
+    [
+        # Case 1: Shallow merge (no recursion)
+        (
+            {"key": "base_value"},
+            {"key": "overlay_value"},
+            False,
+            {"key": "overlay_value"},
+            {"key"},
+            set(),
+        ),
+        # Case 2: Deep merge (nested dictionaries)
+        (
+            {"key": {"subkey": "base_value"}},
+            {"key": {"subkey": "overlay_value"}},
+            True,
+            {"key": {"subkey": "overlay_value"}},
+            {"key.subkey"},
+            set(),
+        ),
+        # Case 3: Overwriting a top-level key (non-nested)
+        (
+            {"key": "base_value"},
+            {"key": "overlay_value"},
+            True,
+            {"key": "overlay_value"},
+            {"key"},
+            set(),
+        ),
+        # Case 4: Overwriting a nested key (deep merge)
+        (
+            {"key": {"subkey": "base_value", "subkey2": "base_value2"}},
+            {"key": {"subkey": "overlay_value"}},
+            True,
+            {"key": {"subkey": "overlay_value", "subkey2": "base_value2"}},
+            {"key.subkey"},
+            {"key.subkey2"},
+        ),
+        # Case 5: No overlay values, so base_dict remains the same
+        (
+            {"key": "base_value"},
+            {},
+            False,
+            {"key": "base_value"},
+            set(),
+            {"key"},
+        ),
+    ],
+)
+# "base_dict, overlay_dict, deep_merge, expected_merged, expected_overwritten, expected_skipped",
+def test_deep_merge_dicts(base_dict, overlay_dict, deep_merge, expected_merged, expected_overwritten, expected_skipped):
+    merged_dict, overwritten_keys, skipped_keys = deep_merge_dicts(base_dict, overlay_dict, deep_merge)
+
+    assert overwritten_keys == expected_overwritten
+    assert skipped_keys == expected_skipped
+    assert merged_dict == expected_merged

41807e6d61cb5e1a66884839761a4d37b9c27437
Javier Torres <javier@mozilla.ai>
2025-03-14 11:41:36 +0100

    Fix lint errors

--- lumigator/backend/backend/alembic/versions/76bbe19c945c_move_to_non_native_enums_for_.py
+++ lumigator/backend/backend/alembic/versions/76bbe19c945c_move_to_non_native_enums_for_.py
@@ -5,17 +5,16 @@ Revises: da3992fd274e
 Create Date: 2025-03-13 09:52:33.090037
 
 """
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '76bbe19c945c' # pragma: allowlist secret
-down_revision: Union[str, None] = 'da3992fd274e' # pragma: allowlist secret
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | None = 'da3992fd274e' # pragma: allowlist secret
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:

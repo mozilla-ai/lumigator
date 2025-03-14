6ab996db0313de5840d72e5b33e19501e6770920
Peter Wilson <peter@mozilla.ai>
2025-03-14 21:32:54 +0000

    Prevent things in the cache or tests folder (backend) from restarting the app

--- lumigator/backend/backend/tests/integration/api/routes/test_api_workflows.py
+++ lumigator/backend/backend/tests/integration/api/routes/test_api_workflows.py
@@ -373,6 +373,7 @@ def test_full_experiment_launch(
     workflow_1 = run_workflow(local_client, experiment_id, "Workflow_1", model)
     workflow_1_details = wait_for_workflow_complete(local_client, workflow_1.id)
     assert workflow_1_details.artifacts_download_url
+    print(f"... workflow_1_details.artifacts_download_url: {workflow_1_details.artifacts_download_url}")
     check_artifacts_times(workflow_1_details.artifacts_download_url)
     validate_experiment_results(local_client, experiment_id, workflow_1_details)
     workflow_2 = run_workflow(local_client, experiment_id, "Workflow_2", model)

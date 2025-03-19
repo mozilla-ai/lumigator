78de1082a1d0a5162eb2f01d2fe14103c3bddd53
Khaled Osman <khaled.osmaan@gmail.com>
2025-03-19 10:49:43 +0100

    banner styling

--- lumigator/frontend/src/components/common/ConsentBanner.vue
+++ lumigator/frontend/src/components/common/ConsentBanner.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="consent-banner">
-    <p>
-      We use Sentry to send anonymized error reports and improve Lumigator. Do you allow us to
-      collect error reports? You can later change your consent from Settings
-    </p>
-    <div class="actions">
-      <PrimeVueButton @click="acceptConsent">Yes</PrimeVueButton>
-      <PrimeVueButton @click="declineConsent">No</PrimeVueButton>
+    <div class="container">
+      <PrimeVueButton
+        class="close-button"
+        icon="pi pi-times"
+        severity="secondary"
+        variant="text"
+        raised
+        rounded
+        aria-label="Cancel"
+      ></PrimeVueButton>
+      <div class="content-wrapper">
+        <h4 class="header">Data Usage</h4>
+        <p class="text">
+          To help improve Lumigator, you can choose to share completely anonymized usage data. We do
+          not profile you or track your location or web activity. Do you allow us to collect this
+          data? You can always change your choice in Settings.
+        </p>
+        <div class="actions">
+          <PrimeVueButton size="small" severity="secondary" @click="declineConsent" rounded
+            >No</PrimeVueButton
+          >
+          <PrimeVueButton size="small" @click="acceptConsent" rounded>Yes</PrimeVueButton>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -39,20 +56,61 @@ export default {
 
 <style scoped lang="scss">
 @use '@/styles/variables';
+@use '@/styles/mixins';
 
 .consent-banner {
   position: fixed;
   bottom: 0;
-  width: 100%;
-  background-color: variables.$l-main-bg;
   color: variables.$l-grey-100;
-  padding: 10px;
   text-align: center;
+
+  display: flex;
+  width: 100%;
+  padding: 0.5rem;
+  justify-content: center;
+  align-items: center;
+
+  border-top: 0.5px solid variables.$border-stroke;
+  background: variables.$black;
+}
+
+.container {
+  position: relative;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.content-wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  max-width: 31.75rem;
+  padding: 0.5rem 0rem;
+  align-items: center;
+}
+
+.header {
+  @include mixins.paragraph-2;
+  color: variables.$white;
+}
+
+.text {
+  color: variables.$l-grey-100;
+
+  @include mixins.paragraph;
 }
 
 .actions {
   display: flex;
   justify-content: center;
   gap: 1rem;
+}
+
+.close-button {
+  position: absolute;
+  margin-right: 2rem;
+  top: 0;
+  right: 0;
 }
 </style>
